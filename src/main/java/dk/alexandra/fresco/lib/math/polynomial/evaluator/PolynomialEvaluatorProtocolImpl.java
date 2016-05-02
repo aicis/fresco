3ca3a7c8452d1dcdda6a846e2eb682064d86e725
Jonas Lindstrøm Jensen <jonas.l.jensen@alexandra.dk>
2016-05-02 11:37:41 +0200

    More robust code in case some coefficients are not initialised.

--- src/main/java/dk/alexandra/fresco/lib/math/polynomial/evaluator/PolynomialEvaluatorProtocolImpl.java
+++ src/main/java/dk/alexandra/fresco/lib/math/polynomial/evaluator/PolynomialEvaluatorProtocolImpl.java
@@ -9,38 +9,48 @@ import dk.alexandra.fresco.lib.helper.builder.NumericProtocolBuilder;
 import dk.alexandra.fresco.lib.helper.sequential.SequentialProtocolProducer;
 import dk.alexandra.fresco.lib.math.polynomial.Polynomial;
 
-public class PolynomialEvaluatorProtocolImpl extends AbstractSimpleProtocol implements PolynomialEvaluatorProtocol {
+public class PolynomialEvaluatorProtocolImpl extends AbstractSimpleProtocol implements
+		PolynomialEvaluatorProtocol {
 
 	private final SInt x;
 	private final Polynomial p;
 	private SInt result;
 	private final BasicNumericFactory basicNumericFactory;
 
-	public PolynomialEvaluatorProtocolImpl(SInt x, Polynomial p, SInt result, BasicNumericFactory basicNumericFactory) {
+	public PolynomialEvaluatorProtocolImpl(SInt x, Polynomial p, SInt result,
+			BasicNumericFactory basicNumericFactory) {
 		this.x = x;
 		this.p = p;
 		this.result = result;
 		this.basicNumericFactory = basicNumericFactory;
 	}
-	
+
 	@Override
 	protected ProtocolProducer initializeGateProducer() {
-		
+
 		NumericProtocolBuilder builder = new NumericProtocolBuilder(basicNumericFactory);
-		
+
 		int degree = p.getMaxDegree();
-		
+
 		/*
-		 * We use Horner's method, p(x) = (( ... ((p_{n-1} x + p_{n-2})x + p_{n-3}) ... )x + a_1)x + a_0
+		 * We use Horner's method, p(x) = (( ... ((p_{n-1} x + p_{n-2})x +
+		 * p_{n-3}) ... )x + a_1)x + a_0
 		 */
 		SInt tmp = p.getCoefficient(degree - 1);
 		builder.beginSeqScope();
+		if (tmp == null) {
+			tmp = builder.getSInt(0);
+		}
 		for (int i = degree - 2; i >= 0; i--) {
-			tmp = builder.add(builder.mult(tmp, x), p.getCoefficient(i));
+			tmp = builder.mult(tmp, x);
+			if (p.getCoefficient(i) != null) {
+				tmp = builder.add(tmp, p.getCoefficient(i));
+			}
 		}
 		builder.endCurScope();
-		
-		return new SequentialProtocolProducer(builder.getCircuit(), new CopyProtocolImpl<SInt>(tmp, result));
+
+		return new SequentialProtocolProducer(builder.getCircuit(), new CopyProtocolImpl<SInt>(tmp,
+				result));
 	}
 
 }
