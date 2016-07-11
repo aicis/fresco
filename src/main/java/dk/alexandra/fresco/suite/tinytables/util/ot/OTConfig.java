b19fcba067fc451f092b5467234d8545581a55df
Jonas Lindstrøm Jensen <jonas.l.jensen@alexandra.dk>
2016-07-11 14:54:48 +0200

    Add link to installation guide for Scapi

--- src/main/java/dk/alexandra/fresco/suite/tinytables/util/ot/OTConfig.java
+++ src/main/java/dk/alexandra/fresco/suite/tinytables/util/ot/OTConfig.java
@@ -10,9 +10,12 @@ public class OTConfig {
 	public static final String OT_SENDER = "dk/alexandra/fresco/suite/tinytables/util/ot/scapi/NativeOTSender";
 	
 	/*
-	 * Note that SCAPI needs to be installed. Actually the scapi command is just
-	 * a script which adds Apache Commons, Scapi and BouncyCastle to the
-	 * classpath, so we should be able to do this with a 'java' command instead.
+	 * Note that SCAPI needs to be installed (see
+	 * http://scapi.readthedocs.io/en/latest/install.html).
+	 * 
+	 * TODO: Actually the scapi command is just a script which adds Apache
+	 * Commons, Scapi and BouncyCastle to the classpath, so we should be able to
+	 * do this with a 'java' command instead.
 	 */
 	public static final String SCAPI_CMD = "/usr/local/bin/scapi";
 	
