aicis/fresco

commit 3c5fc5bd3c2d8d721146a1562219ecb0a3acdce5

Should use setShare in order to correctly set the ready field

src/main/java/dk/alexandra/fresco/suite/tinytables/prepro/datatypes/TinyTablesPreproSBool.java
@@ -49,7 +49,7 @@ public class TinyTablesPreproSBool implements SBool {
 	}
 
 	public TinyTablesPreproSBool() {
-		this.share = false;
+		this(false);
 	}
 
 	@Override

commit aece69a12680d953c53c8f0ca553b5f3188a0baf

Fix bug introduced in 3c5fc5bd3c2d8d721146a1562219ecb0a3acdce5. SBools are not ready until the share has been set, and NOT when created using the default constructor.

src/main/java/dk/alexandra/fresco/suite/tinytables/prepro/datatypes/TinyTablesPreproSBool.java
@@ -49,7 +49,7 @@ public class TinyTablesPreproSBool implements SBool {
 	}
 
 	public TinyTablesPreproSBool() {
-		this(false);
+		// Not ready yet
 	}
 
 	@Override