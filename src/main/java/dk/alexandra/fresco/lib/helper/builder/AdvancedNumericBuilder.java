5d2495ac29424aba554a589be67ab9763e53b195
Spanbroek, Mark <mark@spanbroek.net>
2017-03-02 11:40:38 +0100

    Add javadoc and rename division parameters to make intent more clear.

--- src/main/java/dk/alexandra/fresco/lib/helper/builder/AdvancedNumericBuilder.java
+++ src/main/java/dk/alexandra/fresco/lib/helper/builder/AdvancedNumericBuilder.java
@@ -15,9 +15,15 @@ public class AdvancedNumericBuilder extends AbstractProtocolBuilder {
         this.intFactory = intFactory;
     }
 
-    public SInt div(SInt p, int maxBitLengthP, SInt q, int maxBitLengthQ) {
+    /**
+     * Integer division of two shared integers.
+     * @param maxNumeratorLength the maximum amount of bits in the numerator.
+     * @param maxDenominatorLength the maximum amount of bits in the denominator.
+     * @return <code>numerator / denominator</code>
+     */
+    public SInt div(SInt numerator, int maxNumeratorLength, SInt denominator, int maxDenominatorLength) {
         SInt out = intFactory.getSInt();
-        append(divisionFactory.getDivisionProtocol(p, maxBitLengthP, q, maxBitLengthQ, out));
+        append(divisionFactory.getDivisionProtocol(numerator, maxNumeratorLength, denominator, maxDenominatorLength, out));
         return out;
     }
 
