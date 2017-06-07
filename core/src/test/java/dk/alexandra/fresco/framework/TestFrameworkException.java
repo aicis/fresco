1fddfce37411f76d6def0c9635abc1d2d165b4f1
Peter F. Frandsen <pff@partisia.com>
2017-06-07 20:25:29 +0200

    Corrected error collection

--- core/src/test/java/dk/alexandra/fresco/framework/TestFrameworkException.java
+++ core/src/test/java/dk/alexandra/fresco/framework/TestFrameworkException.java
@@ -29,18 +29,17 @@ package dk.alexandra.fresco.framework;
 /**
  * An exception thrown when exceptions happen in test framework, e.g. during
  * setUp or tearDown of test fixtures, etc.
- * 
  */
 public class TestFrameworkException extends RuntimeException {
 
-	private static final long serialVersionUID = -7610884868224471086L;
-	
-	public TestFrameworkException(String msg) {
-		super(msg);
-	}
-	
-	public TestFrameworkException(String msg, Exception e) {
-		super(msg, e);
-	}
+  private static final long serialVersionUID = -7610884868224471086L;
+
+  public TestFrameworkException(String msg) {
+    super(msg);
+  }
+
+  public TestFrameworkException(String msg, Throwable e) {
+    super(msg, e);
+  }
 
 }
