e44c8351680eb4bdccec37eeacb782a3495a981e
Emil Bremer Orloff <ebo@partisia.com>
2017-06-21 14:14:48 +0200

    temp

--- core/src/main/java/dk/alexandra/fresco/lib/math/integer/binary/RightShiftProtocol4.java
+++ core/src/main/java/dk/alexandra/fresco/lib/math/integer/binary/RightShiftProtocol4.java
@@ -31,7 +31,6 @@ import dk.alexandra.fresco.framework.RightShiftBuilder.RightShiftResult;
 import dk.alexandra.fresco.framework.builder.NumericBuilder;
 import dk.alexandra.fresco.framework.builder.ProtocolBuilder.SequentialProtocolBuilder;
 import dk.alexandra.fresco.framework.builder.RandomAdditiveMaskBuilder;
-import dk.alexandra.fresco.framework.builder.RandomAdditiveMaskBuilder.RandomAdditiveMask;
 import dk.alexandra.fresco.framework.util.Pair;
 import dk.alexandra.fresco.framework.value.OInt;
 import dk.alexandra.fresco.framework.value.OIntFactory;
@@ -67,40 +66,34 @@ public class RightShiftProtocol4<SIntT extends SInt>
 
   @Override
   public Computation<RightShiftResult<SIntT>> apply(SequentialProtocolBuilder<SIntT> sequential) {
-    Computation<RandomAdditiveMask<SIntT>> mask = sequential
-        .createSequentialSubFactoryReturning((builder) -> {
-          RandomAdditiveMaskBuilder<SIntT> additiveMaskBuilder = builder
-              .createAdditiveMaskBuilder();
-          return additiveMaskBuilder.additiveMask(bitLength);
-        });
-    Computation<Pair<Computation<Pair<Computation<SIntT>, Computation<SIntT>>>, Computation<OInt>>> preprocess =
-        sequential.createParallelSubFactoryReturning((parallel) -> {
-          Computation<Pair<Computation<SIntT>, Computation<SIntT>>> topAndBottom =
-              parallel.createSequentialSubFactoryReturning((parSubSequential) -> {
-                OInt two = parSubSequential.getOIntFactory().getOInt(BigInteger.valueOf(2));
-                NumericBuilder<SIntT> numericBuilder = parSubSequential.numeric();
-                Computation<? extends OInt> inverseOfTwo = numericBuilder.invert(two);
-                RandomAdditiveMask<SIntT> randomAdditiveMask = mask.out();
-                Computation<SIntT> rBottom = () -> randomAdditiveMask.bits.get(0);
-                Computation<SIntT> sub = numericBuilder.sub(() -> randomAdditiveMask.r, rBottom);
-                Computation<SIntT> rTop = numericBuilder.mult(inverseOfTwo.out(), sub);
-                return () -> new Pair<>(rBottom, rTop);
-              });
-          Computation<OInt> maskOpen =
-              parallel.createSequentialSubFactoryReturning((parSubSequential) -> {
-                NumericBuilder<SIntT> numericBuilder = parSubSequential.numeric();
-                Computation<SIntT> result = numericBuilder.add(input, () -> mask.out().r);
-                return parSubSequential.createOpenBuilder().open(result);
-              });
-          return () -> new Pair<>(topAndBottom, maskOpen);
-        });
-    return sequential.createSequentialSubFactoryReturning((round1) -> {
-      Pair<Computation<Pair<Computation<SIntT>, Computation<SIntT>>>, Computation<OInt>> preprocessOutput =
-          preprocess.out();
-
-      Computation<OInt> mOpen = preprocessOutput.getSecond();
-      Computation<SIntT> rBottom = preprocessOutput.getFirst().out().getFirst();
-      Computation<SIntT> rTop = preprocessOutput.getFirst().out().getSecond();
+    return sequential.seq((builder) -> {
+      RandomAdditiveMaskBuilder<SIntT> additiveMaskBuilder = builder
+          .createAdditiveMaskBuilder();
+      return additiveMaskBuilder.additiveMask(bitLength);
+    }).par((randomAdditiveMask, parallel) -> {
+      Computation<Pair<Computation<SIntT>, Computation<SIntT>>> topAndBottom =
+          parallel.createSequentialSubFactoryReturning((parSubSequential) -> {
+            OInt two = parSubSequential.getOIntFactory().getOInt(BigInteger.valueOf(2));
+            NumericBuilder<SIntT> numericBuilder = parSubSequential.numeric();
+            Computation<? extends OInt> inverseOfTwo = numericBuilder.invert(two);
+            Computation<SIntT> rBottom = () -> randomAdditiveMask.bits.get(0);
+            Computation<SIntT> sub = numericBuilder
+                .sub(() -> randomAdditiveMask.r, rBottom);
+            Computation<SIntT> rTop = numericBuilder.mult(inverseOfTwo.out(), sub);
+            return () -> new Pair<>(rBottom, rTop);
+          });
+      Computation<OInt> maskOpen =
+          parallel.createSequentialSubFactoryReturning((parSubSequential) -> {
+            NumericBuilder<SIntT> numericBuilder = parSubSequential.numeric();
+            Computation<SIntT> result = numericBuilder
+                .add(input, () -> randomAdditiveMask.r);
+            return parSubSequential.createOpenBuilder().open(result);
+          });
+      return () -> new Pair<>(topAndBottom.out(), maskOpen.out());
+    }).seq((preprocessOutput, round1) -> {
+      OInt mOpen = preprocessOutput.getSecond();
+      Computation<SIntT> rBottom = preprocessOutput.getFirst().getFirst();
+      Computation<SIntT> rTop = preprocessOutput.getFirst().getSecond();
           /*
            * 'carry' is either 0 or 1. It is 1 if and only if the
 					 * addition m = x + r gave a carry from the first (least
@@ -110,68 +103,70 @@ public class RightShiftProtocol4<SIntT extends SInt>
 					 * is equal to r_0 * (m + 1 (mod 2)).
 					 */
       NumericBuilder<SIntT> numericBuilder = round1.numeric();
-      OInt mBottomNegated = round1.getOIntFactory().getOInt(mOpen.out().getValue()
+      OInt mBottomNegated = round1.getOIntFactory().getOInt(mOpen.getValue()
           .add(BigInteger.ONE).mod(BigInteger.valueOf(2)));
       Computation<SIntT> carry = numericBuilder.mult(mBottomNegated, rBottom);
-      return round1.createParallelSubFactoryReturning((parallel) -> {
-        // The carry is needed by both the calculation of the shift
-        // and the remainder, but the shift and the remainder can be
-        // calculated in parallel.
-        Computation<SIntT> shifted = parallel
-            .createSequentialSubFactoryReturning((parSubSequential) -> {
-              NumericBuilder<SIntT> parSubSeqNumericBuilder = parSubSequential
-                  .numeric();
-              BigInteger openShiftOnce = mOpen.out().getValue().shiftRight(1);
-              OInt mTop = parSubSequential.getOIntFactory().getOInt(openShiftOnce);
-              // Now we calculate the shift, x >> 1 = mTop - rTop - carry
-              Computation<SIntT> sub = parSubSeqNumericBuilder.sub(mTop, rTop);
-              return parSubSeqNumericBuilder.sub(sub, carry);
-            });
-        List<Computation<SIntT>> remainders;
-        if (calculateRemainders) {
-            /*
-             * We also need to calculate the remainder, aka. the bit
-						 * we throw away in the shift: x (mod 2) = xor(r_0, m
-						 * mod 2) = r_0 + (m mod 2) - 2 (r_0 * (m mod 2)).
-						 */
-          remainders =
-              Collections.singletonList(
-                  parallel.createSequentialSubFactoryReturning((parSubSequential) -> {
-                    OIntFactory oIntFactory = parSubSequential.getOIntFactory();
+      return round1.par((parallel) -> {
+            // The carry is needed by both the calculation of the shift
+            // and the remainder, but the shift and the remainder can be
+            // calculated in parallel.
+            Computation<SIntT> shifted =
+                parallel.createSequentialSubFactoryReturning((parSubSequential) -> {
+                  NumericBuilder<SIntT> parSubSeqNumericBuilder = parSubSequential
+                      .numeric();
+                  BigInteger openShiftOnce = mOpen.getValue().shiftRight(1);
+                  OInt mTop = parSubSequential.getOIntFactory().getOInt(openShiftOnce);
+                  // Now we calculate the shift, x >> 1 = mTop - rTop - carry
+                  Computation<SIntT> sub = parSubSeqNumericBuilder.sub(mTop, rTop);
+                  return parSubSeqNumericBuilder.sub(sub, carry);
+                });
+            List<Computation<SIntT>> remainders;
+            if (calculateRemainders) {
+              // We also need to calculate the remainder, aka. the bit
+              // we throw away in the shift:
+              //   x (mod 2) =
+              //     xor(r_0, m mod 2) =
+              //     r_0 + (m mod 2) - 2 (r_0 * (m mod 2)).
+              remainders =
+                  Collections.singletonList(
+                      parallel.createSequentialSubFactoryReturning((parSubSequential) -> {
+                        OIntFactory oIntFactory = parSubSequential.getOIntFactory();
 
-                    OInt mBottom = oIntFactory.getOInt(
-                        mOpen.out().getValue().mod(BigInteger.valueOf(2))
-                    );
-                    OInt twoMBottom = oIntFactory.getOInt(
-                        mBottom.getValue().shiftLeft(1)
-                    );
-
-                    Computation<Pair<Computation<SIntT>, Computation<SIntT>>> productAndSum =
-                        parSubSequential.createParallelSubFactoryReturning(
-                            (productAndSumBuilder) -> {
-                              NumericBuilder<SIntT> productAndSumNumeric =
-                                  productAndSumBuilder.numeric();
-                              Computation<SIntT> product = productAndSumNumeric
-                                  .mult(twoMBottom, rBottom);
-                              Computation<SIntT> sum = productAndSumNumeric.add(mBottom, rBottom);
-                              return () -> new Pair<>(product, sum);
-                            }
+                        OInt mBottom = oIntFactory.getOInt(
+                            mOpen.getValue().mod(BigInteger.valueOf(2))
+                        );
+                        OInt twoMBottom = oIntFactory.getOInt(
+                            mBottom.getValue().shiftLeft(1)
                         );
 
-                    return parSubSequential.createSequentialSubFactoryReturning((finalBuilder) -> {
-                      NumericBuilder<SIntT> finalNumeric =
-                          finalBuilder.numeric();
-                      return finalNumeric.sub(
-                          productAndSum.out().getSecond(),
-                          productAndSum.out().getFirst());
-                    });
-                  }));
-        } else {
-          remainders = null;
-        }
-        return () -> new RightShiftResult<SIntT>(shifted, remainders);
-      });
+                        Computation<Pair<Computation<SIntT>, Computation<SIntT>>> productAndSum =
+                            parSubSequential.createParallelSubFactoryReturning(
+                                (productAndSumBuilder) -> {
+                                  NumericBuilder<SIntT> productAndSumNumeric =
+                                      productAndSumBuilder.numeric();
+                                  Computation<SIntT> product = productAndSumNumeric
+                                      .mult(twoMBottom, rBottom);
+                                  Computation<SIntT> sum = productAndSumNumeric
+                                      .add(mBottom, rBottom);
+                                  return () -> new Pair<>(product, sum);
+                                }
+                            );
+
+                        return parSubSequential
+                            .createSequentialSubFactoryReturning((finalBuilder) -> {
+                              NumericBuilder<SIntT> finalNumeric =
+                                  finalBuilder.numeric();
+                              return finalNumeric.sub(
+                                  productAndSum.out().getSecond(),
+                                  productAndSum.out().getFirst());
+                            });
+                      }));
+            } else {
+              remainders = null;
+            }
+            return () -> new RightShiftResult<>(shifted, remainders);
+          }
+      );
     });
   }
-
 }
