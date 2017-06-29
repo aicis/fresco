737f0dd14e01a3f8d2fe4e5eefa7e4e96424943d
Peter F. Frandsen <pff@partisia.com>
2017-06-29 18:09:22 +0200

    Made sure the sequential is explicit

--- suite/spdz/src/main/java/dk/alexandra/fresco/suite/spdz/SpdzProtocolSuite.java
+++ suite/spdz/src/main/java/dk/alexandra/fresco/suite/spdz/SpdzProtocolSuite.java
@@ -27,13 +27,13 @@
 package dk.alexandra.fresco.suite.spdz;
 
 import dk.alexandra.fresco.framework.BuilderFactory;
-import dk.alexandra.fresco.framework.builder.ProtocolBuilderNumeric.SequentialProtocolBuilder;
+import dk.alexandra.fresco.framework.builder.ProtocolBuilderNumeric;
 import dk.alexandra.fresco.suite.ProtocolSuite;
 import dk.alexandra.fresco.suite.spdz.configuration.SpdzConfiguration;
 import dk.alexandra.fresco.suite.spdz.utils.SpdzFactory;
 
 public class SpdzProtocolSuite implements
-    ProtocolSuite<SpdzResourcePool, SequentialProtocolBuilder> {
+    ProtocolSuite<SpdzResourcePool, ProtocolBuilderNumeric.SequentialProtocolBuilder> {
 
   private SpdzConfiguration spdzConf;
 
@@ -42,7 +42,8 @@ public class SpdzProtocolSuite implements
   }
 
   @Override
-  public BuilderFactory<SequentialProtocolBuilder> init(SpdzResourcePool resourcePool) {
+  public BuilderFactory<ProtocolBuilderNumeric.SequentialProtocolBuilder> init(
+      SpdzResourcePool resourcePool) {
     int maxBitLength = spdzConf.getMaxBitLength();
     return new SpdzBuilder(
         new SpdzFactory(resourcePool.getStore(), resourcePool.getMyId(), maxBitLength));
