ccf05753c93c9bb3d1ea306d1f39cae5b65ffcbc
Peter F. Frandsen <pff@partisia.com>
2017-06-30 16:36:55 +0200

    Fixed compile problem

--- core/src/main/java/dk/alexandra/fresco/lib/math/polynomial/evaluator/PolynomialEvaluator.java
+++ core/src/main/java/dk/alexandra/fresco/lib/math/polynomial/evaluator/PolynomialEvaluator.java
@@ -28,39 +28,39 @@ package dk.alexandra.fresco.lib.math.polynomial.evaluator;
 
 import dk.alexandra.fresco.framework.Computation;
 import dk.alexandra.fresco.framework.builder.ComputationBuilder;
-import dk.alexandra.fresco.framework.builder.ProtocolBuilderNumeric.SequentialProtocolBuilder;
+import dk.alexandra.fresco.framework.builder.ProtocolBuilderNumeric.SequentialNumericBuilder;
 import dk.alexandra.fresco.framework.value.SInt;
 import dk.alexandra.fresco.lib.math.polynomial.Polynomial;
 
 public class PolynomialEvaluator implements ComputationBuilder<SInt> {
 
-	private final Computation<SInt> x;
-	private final Polynomial p;
+  private final Computation<SInt> x;
+  private final Polynomial p;
 
-	public PolynomialEvaluator(Computation<SInt> x, Polynomial p) {
-		this.x = x;
-		this.p = p;
-	}
+  public PolynomialEvaluator(Computation<SInt> x, Polynomial p) {
+    this.x = x;
+    this.p = p;
+  }
 
 
-	@Override
-	public Computation<SInt> build(SequentialProtocolBuilder builder) {
-		int degree = p.getMaxDegree();
+  @Override
+  public Computation<SInt> build(SequentialNumericBuilder builder) {
+    int degree = p.getMaxDegree();
 
 		/*
-		 * We use Horner's method, p(x) = (( ... ((p_{n-1} x + p_{n-2})x +
+     * We use Horner's method, p(x) = (( ... ((p_{n-1} x + p_{n-2})x +
 		 * p_{n-3}) ... )x + a_1)x + a_0
 		 */
-		Computation<SInt> tmp = p.getCoefficient(degree - 1);
+    Computation<SInt> tmp = p.getCoefficient(degree - 1);
 
-		for (int i = degree - 2; i >= 0; i--) {
-			tmp = builder.numeric().mult(tmp, x);
-			if (p.getCoefficient(i) != null) {
-				tmp = builder.numeric().add(tmp, p.getCoefficient(i));
-			}
-		}
+    for (int i = degree - 2; i >= 0; i--) {
+      tmp = builder.numeric().mult(tmp, x);
+      if (p.getCoefficient(i) != null) {
+        tmp = builder.numeric().add(tmp, p.getCoefficient(i));
+      }
+    }
 
-		return tmp;
-	}
+    return tmp;
+  }
 
 }
