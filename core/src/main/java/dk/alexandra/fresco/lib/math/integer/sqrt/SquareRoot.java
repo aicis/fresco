4fa778d3425424880c699ec7a90b3c74cd243aea
Peter Sebastian Nordholt <peter.s.nordholt@alexandra.dk>
2017-07-28 11:38:45 +0200

    Some formating

--- core/src/main/java/dk/alexandra/fresco/lib/math/integer/sqrt/SquareRoot.java
+++ core/src/main/java/dk/alexandra/fresco/lib/math/integer/sqrt/SquareRoot.java
@@ -3,27 +3,25 @@
  *
  * This file is part of the FRESCO project.
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
  *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
  *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+ * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  *
- * FRESCO uses SCAPI - http://crypto.biu.ac.il/SCAPI, Crypto++, Miracl, NTL,
- * and Bouncy Castle. Please see these projects for any further licensing issues.
+ * FRESCO uses SCAPI - http://crypto.biu.ac.il/SCAPI, Crypto++, Miracl, NTL, and Bouncy Castle.
+ * Please see these projects for any further licensing issues.
  */
+
 package dk.alexandra.fresco.lib.math.integer.sqrt;
 
 import dk.alexandra.fresco.framework.Computation;
@@ -34,9 +32,9 @@ import dk.alexandra.fresco.framework.value.SInt;
 import java.math.BigInteger;
 
 /**
- * This class implements a protocol for approximating the square root of a
- * secret shared integer using the <a href=
- * "https://en.wikipedia.org/wiki/Methods_of_computing_square_roots#Babylonian_method"
+ * This class implements a protocol for approximating the square root of a secret shared integer
+ * using the
+ * <a href= "https://en.wikipedia.org/wiki/Methods_of_computing_square_roots#Babylonian_method"
  * >Babylonian Method</a>.
  *
  * @author Jonas Lindstrøm (jonas.lindstrom@alexandra.dk)
@@ -56,44 +54,34 @@ public class SquareRoot implements ComputationBuilder<SInt> {
   @Override
   public Computation<SInt> build(SequentialNumericBuilder builder) {
     /*
-     * Convergence is quadratic (the number of correct digits rougly doubles
-		 * on each iteration) so assuming we have at least one digit correct
-		 * after first iteration, we need at about log2(maxInputLength)
-		 * iterations in total.
-		 */
+     * Convergence is quadratic (the number of correct digits rougly doubles on each iteration) so
+     * assuming we have at least one digit correct after first iteration, we need at about
+     * log2(maxInputLength) iterations in total.
+     */
     int iterations = log2(maxInputLength) + 1;
 
-	/*
+    /*
      * First guess is 2 ^ (bitlength / 2)
-	 */
+     */
     return builder.seq((seq) -> {
-      Computation<SInt> bitlength = seq.advancedNumeric()
-              .bitLength(input, maxInputLength);
-      Computation<SInt> halfBitlength = seq.advancedNumeric()
-              .rightShift(bitlength);
-      Computation<SInt> estimate = seq.advancedNumeric()
-              .exp(BigInteger.valueOf(2), halfBitlength, BigInteger.valueOf(maxInputLength).bitLength());
+      Computation<SInt> bitlength = seq.advancedNumeric().bitLength(input, maxInputLength);
+      Computation<SInt> halfBitlength = seq.advancedNumeric().rightShift(bitlength);
+      Computation<SInt> estimate = seq.advancedNumeric().exp(BigInteger.valueOf(2), halfBitlength,
+          BigInteger.valueOf(maxInputLength).bitLength());
       return new IterationState(1, estimate);
       /*
-      * We iterate y[n+1] = (y[n] + x / y[n]) / 2.
-		  */
-    }).whileLoop(
-        (iterationState) ->
-            iterationState.iteration < iterations,
+       * We iterate y[n+1] = (y[n] + x / y[n]) / 2.
+       */
+    }).whileLoop((iterationState) -> iterationState.iteration < iterations,
         (iterationState, seq) -> {
           Computation<SInt> value = iterationState.value;
           AdvancedNumericBuilder advancedNumeric = seq.advancedNumeric();
 
-          Computation<SInt> quotient = advancedNumeric.div(
-              input, value
-          );
+          Computation<SInt> quotient = advancedNumeric.div(input, value);
           Computation<SInt> sum = seq.numeric().add(value, quotient);
           Computation<SInt> updatedValue = seq.advancedNumeric().rightShift(sum);
           return new IterationState(iterationState.iteration + 1, updatedValue);
-        }
-    ).seq((iterationState, seq) ->
-        iterationState.value
-    );
+        }).seq((iterationState, seq) -> iterationState.value);
   }
 
   /**
@@ -108,8 +96,7 @@ public class SquareRoot implements ComputationBuilder<SInt> {
     private final int iteration;
     private final Computation<SInt> value;
 
-    private IterationState(int iteration,
-        Computation<SInt> value) {
+    private IterationState(int iteration, Computation<SInt> value) {
       this.iteration = iteration;
       this.value = value;
     }
