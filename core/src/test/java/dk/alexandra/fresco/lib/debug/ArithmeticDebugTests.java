19982fb556c912b3e047ea3a3c2ae842ae2d8f7c
Peter F. Frandsen <pff@partisia.com>
2017-08-25 13:54:25 +0200

    Fixed test for unix

--- core/src/test/java/dk/alexandra/fresco/lib/debug/ArithmeticDebugTests.java
+++ core/src/test/java/dk/alexandra/fresco/lib/debug/ArithmeticDebugTests.java
@@ -36,39 +36,40 @@ public class ArithmeticDebugTests {
           Application<Void, SequentialNumericBuilder> app =
               new Application<Void, SequentialNumericBuilder>() {
 
-            @Override
-            public Computation<Void> prepareApplication(SequentialNumericBuilder producer) {
-              return producer.seq(seq -> {
-                List<Computation<SInt>> toPrint =
-                    seq.numeric().known(Arrays.asList(new BigInteger[] {BigInteger.ONE,
-                        BigInteger.TEN, BigInteger.ZERO, BigInteger.ONE}));
-                return () -> toPrint;
-              }).seq((inputs, seq) -> {
-                seq.utility().openAndPrint("testNumber", inputs.get(0), stream);
-                seq.utility().openAndPrint("testVector", inputs, stream);
-                ArrayList<Computation<SInt>> r1 = new ArrayList<>();
-                r1.add(inputs.get(0));
-                r1.add(inputs.get(1));
-                ArrayList<Computation<SInt>> r2 = new ArrayList<>();
-                r2.add(inputs.get(2));
-                r2.add(inputs.get(3));
-                ArrayList<ArrayList<Computation<SInt>>> m = new ArrayList<>();
-                m.add(r1);
-                m.add(r2);
-                Matrix<Computation<SInt>> matrix = new Matrix<>(2, 2, m);
-                seq.utility().openAndPrint("testMatrix", matrix, stream);
-                return null;
-              });
-            }
+                @Override
+                public Computation<Void> prepareApplication(SequentialNumericBuilder producer) {
+                  return producer.seq(seq -> {
+                    List<Computation<SInt>> toPrint =
+                        seq.numeric().known(Arrays.asList(new BigInteger[]{BigInteger.ONE,
+                            BigInteger.TEN, BigInteger.ZERO, BigInteger.ONE}));
+                    return () -> toPrint;
+                  }).seq((inputs, seq) -> {
+                    seq.utility().openAndPrint("testNumber", inputs.get(0), stream);
+                    seq.utility().openAndPrint("testVector", inputs, stream);
+                    ArrayList<Computation<SInt>> r1 = new ArrayList<>();
+                    r1.add(inputs.get(0));
+                    r1.add(inputs.get(1));
+                    ArrayList<Computation<SInt>> r2 = new ArrayList<>();
+                    r2.add(inputs.get(2));
+                    r2.add(inputs.get(3));
+                    ArrayList<ArrayList<Computation<SInt>>> m = new ArrayList<>();
+                    m.add(r1);
+                    m.add(r2);
+                    Matrix<Computation<SInt>> matrix = new Matrix<>(2, 2, m);
+                    seq.utility().openAndPrint("testMatrix", matrix, stream);
+                    return null;
+                  });
+                }
 
-          };
+              };
 
           secureComputationEngine.runApplication(app,
               ResourcePoolCreator.createResourcePool(conf.sceConf));
 
           String output = bytes.toString("UTF-8");
           Assert.assertEquals(
-              "testNumber\n1\ntestVector\n1, 10, 0, 1, \ntestMatrix\n1, 10, \n0, 1, \n\n", output);
+              "testNumber\n1\ntestVector\n1, 10, 0, 1, \ntestMatrix\n1, 10, \n0, 1, \n\n",
+              output.replace("\r", ""));
         }
       };
     }
