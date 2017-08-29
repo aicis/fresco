61516a17aa1a7746b448262e0e5d71c56a010127
Emil Bremer Orloff <ebo@partisia.com>
2017-08-29 20:47:29 +0200

    Cleaned up statistics tests

--- core/src/test/java/dk/alexandra/fresco/lib/math/integer/stat/StatisticsTests.java
+++ core/src/test/java/dk/alexandra/fresco/lib/math/integer/stat/StatisticsTests.java
@@ -23,19 +23,17 @@
  *
  * FRESCO uses SCAPI - http://crypto.biu.ac.il/SCAPI, Crypto++, Miracl, NTL,
  * and Bouncy Castle. Please see these projects for any further licensing issues.
- *******************************************************************************/
+ */
 package dk.alexandra.fresco.lib.math.integer.stat;
 
-import dk.alexandra.fresco.framework.BuilderFactory;
+import dk.alexandra.fresco.framework.Application;
 import dk.alexandra.fresco.framework.Computation;
-import dk.alexandra.fresco.framework.ProtocolProducer;
-import dk.alexandra.fresco.framework.TestApplication;
 import dk.alexandra.fresco.framework.TestThreadRunner.TestThread;
 import dk.alexandra.fresco.framework.TestThreadRunner.TestThreadFactory;
-import dk.alexandra.fresco.framework.builder.numeric.BuilderFactoryNumeric;
 import dk.alexandra.fresco.framework.builder.numeric.NumericBuilder;
 import dk.alexandra.fresco.framework.builder.numeric.ProtocolBuilderNumeric;
 import dk.alexandra.fresco.framework.network.ResourcePoolCreator;
+import dk.alexandra.fresco.framework.sce.resources.ResourcePool;
 import dk.alexandra.fresco.framework.value.SInt;
 import java.math.BigInteger;
 import java.util.ArrayList;
@@ -48,24 +46,23 @@ import org.junit.Assert;
 /**
  * Generic test cases for basic finite field operations.
  *
- * Can be reused by a test case for any protocol suite that implements the basic
- * field protocol factory.
+ * Can be reused by a test case for any protocol suite that implements the basic field protocol
+ * factory.
  *
- * TODO: Generic tests should not reside in the runtime package. Rather in
- * mpc.lib or something.
+ * TODO: Generic tests should not reside in the runtime package. Rather in mpc.lib or something.
  */
 public class StatisticsTests {
 
-  public static class TestStatistics extends TestThreadFactory {
+  public static class TestStatistics<ResourcePoolT extends ResourcePool> extends TestThreadFactory {
 
     @Override
     public TestThread next() {
-
-      return new TestThread() {
+      //TODO Should be split into different tests for mean, variance, covariance, covariancematrix
+      return new TestThread<ResourcePoolT, ProtocolBuilderNumeric>() {
         private final List<Integer> data1 = Arrays.asList(543, 520, 532, 497, 450, 432);
         private final List<Integer> data2 = Arrays.asList(432, 620, 232, 337, 250, 433);
         private final List<Integer> data3 = Arrays.asList(80, 90, 123, 432, 145, 606);
-        private final List<Integer> dataMean = Arrays.asList(496,384);
+        private final List<Integer> dataMean = Arrays.asList(496, 384);
 
         private Computation<BigInteger> outputMean1;
         private Computation<BigInteger> outputMean2;
@@ -75,68 +72,60 @@ public class StatisticsTests {
 
         @Override
         public void test() throws Exception {
-          TestApplication app = new TestApplication() {
+          Application<Void, ProtocolBuilderNumeric> app =
+              builder -> {
+                NumericBuilder NumericBuilder = builder.numeric();
+                List<Computation<SInt>> input1 = data1.stream()
+                    .map(BigInteger::valueOf)
+                    .map(NumericBuilder::known)
+                    .collect(Collectors.toList());
+                List<Computation<SInt>> input2 = data2.stream()
+                    .map(BigInteger::valueOf)
+                    .map(NumericBuilder::known)
+                    .collect(Collectors.toList());
+                List<Computation<SInt>> input3 = data3.stream()
+                    .map(BigInteger::valueOf)
+                    .map(NumericBuilder::known)
+                    .collect(Collectors.toList());
 
-            @Override
-            public ProtocolProducer prepareApplication(
-                BuilderFactory factory) {
-              return ProtocolBuilderNumeric
-                  .createApplicationRoot((BuilderFactoryNumeric) factory, (builder) -> {
-                    NumericBuilder NumericBuilder = builder.numeric();
-                    List<Computation<SInt>> input1 = data1.stream()
-                        .map(BigInteger::valueOf)
-                        .map(NumericBuilder::known)
-                        .collect(Collectors.toList());
-                    List<Computation<SInt>> input2 = data2.stream()
-                        .map(BigInteger::valueOf)
-                        .map(NumericBuilder::known)
-                        .collect(Collectors.toList());
-                    List<Computation<SInt>> input3 = data3.stream()
-                        .map(BigInteger::valueOf)
-                        .map(NumericBuilder::known)
-                        .collect(Collectors.toList());
+                List<Computation<SInt>> means = dataMean.stream()
+                    .map(BigInteger::valueOf)
+                    .map(NumericBuilder::known)
+                    .collect(Collectors.toList());
 
-                    List<Computation<SInt>> means = dataMean.stream()
-                        .map(BigInteger::valueOf)
-                        .map(NumericBuilder::known)
-                        .collect(Collectors.toList());
+                Computation<SInt> mean1 = builder
+                    .seq(new Mean(input1));
+                Computation<SInt> mean2 = builder
+                    .seq(new Mean(input2));
+                Computation<SInt> variance = builder
+                    .seq(new Variance(input1, mean1));
+                Computation<SInt> covariance = builder
+                    .seq(new Covariance(input1, input2, mean1, mean2));
+                Computation<List<List<Computation<SInt>>>> covarianceMatrix = builder
+                    .seq(
+                        new CovarianceMatrix(Arrays.asList(input1, input2, input3), means));
 
-                    Computation<SInt> mean1 = builder
-                        .seq(new Mean(input1));
-                    Computation<SInt> mean2 = builder
-                        .seq(new Mean(input2));
-                    Computation<SInt> variance = builder
-                        .seq(new Variance(input1, mean1));
-                    Computation<SInt> covariance = builder
-                        .seq(new Covariance(input1, input2, mean1, mean2));
-                    Computation<List<List<Computation<SInt>>>> covarianceMatrix = builder
-                        .seq(
-                            new CovarianceMatrix(Arrays.asList(input1, input2, input3), means));
-
-                    builder.par((par) -> {
-                      NumericBuilder open = par.numeric();
-                      outputMean1 = open.open(mean1);
-                      outputMean2 = open.open(mean2);
-                      outputVariance = open.open(variance);
-                      outputCovariance = open.open(covariance);
-                      List<List<Computation<SInt>>> covarianceMatrixOut = covarianceMatrix.out();
-                      List<List<Computation<BigInteger>>> openCovarianceMatrix = new ArrayList<>(
-                          covarianceMatrixOut.size());
-                      for (List<Computation<SInt>> computations : covarianceMatrixOut) {
-                        List<Computation<BigInteger>> computationList = new ArrayList<>(
-                            computations.size());
-                        openCovarianceMatrix.add(computationList);
-                        for (Computation<SInt> computation : computations) {
-                          computationList.add(open.open(computation));
-                        }
-                      }
-                      outputCovarianceMatix = openCovarianceMatrix;
-                      return null;
-                    });
-
-                  }).build();
-            }
-          };
+                return builder.par((par) -> {
+                  NumericBuilder open = par.numeric();
+                  outputMean1 = open.open(mean1);
+                  outputMean2 = open.open(mean2);
+                  outputVariance = open.open(variance);
+                  outputCovariance = open.open(covariance);
+                  List<List<Computation<SInt>>> covarianceMatrixOut = covarianceMatrix.out();
+                  List<List<Computation<BigInteger>>> openCovarianceMatrix = new ArrayList<>(
+                      covarianceMatrixOut.size());
+                  for (List<Computation<SInt>> computations : covarianceMatrixOut) {
+                    List<Computation<BigInteger>> computationList = new ArrayList<>(
+                        computations.size());
+                    openCovarianceMatrix.add(computationList);
+                    for (Computation<SInt> computation : computations) {
+                      computationList.add(open.open(computation));
+                    }
+                  }
+                  outputCovarianceMatix = openCovarianceMatrix;
+                  return () -> null;
+                });
+              };
           secureComputationEngine
               .runApplication(app, ResourcePoolCreator.createResourcePool(conf.sceConf));
           BigInteger mean1 = outputMean1.out();
@@ -172,7 +161,7 @@ public class StatisticsTests {
           Assert.assertTrue(isInInterval(mean1, mean1Exact, tolerance));
           Assert.assertTrue(isInInterval(mean2, mean2Exact, tolerance));
           Assert.assertTrue(isInInterval(variance, varianceExact, tolerance));
-          System.out.println(covariance + " "+covarianceExact+" - "+tolerance);
+          System.out.println(covariance + " " + covarianceExact + " - " + tolerance);
           Assert.assertTrue(isInInterval(covariance, covarianceExact, tolerance));
           Assert.assertTrue(
               isInInterval(outputCovarianceMatix.get(0).get(0).out(), varianceExact,
@@ -191,13 +180,13 @@ public class StatisticsTests {
   }
 
 
-
-  public static class TestStatisticsNoMean extends TestThreadFactory {
+  public static class TestStatisticsNoMean<ResourcePoolT extends ResourcePool> extends
+      TestThreadFactory {
 
     @Override
     public TestThread next() {
 
-      return new TestThread() {
+      return new TestThread<ResourcePoolT, ProtocolBuilderNumeric>() {
         private final List<Integer> data1 = Arrays.asList(543, 520, 532, 497, 450, 432);
         private final List<Integer> data2 = Arrays.asList(432, 620, 232, 337, 250, 433);
         private final List<Integer> data3 = Arrays.asList(80, 90, 123, 432, 145, 606);
@@ -208,54 +197,47 @@ public class StatisticsTests {
 
         @Override
         public void test() throws Exception {
-          TestApplication app = new TestApplication() {
-            @Override
-            public ProtocolProducer prepareApplication(
-                BuilderFactory factory) {
-              return ProtocolBuilderNumeric
-                  .createApplicationRoot((BuilderFactoryNumeric) factory, (builder) -> {
-                    NumericBuilder NumericBuilder = builder.numeric();
-                    List<Computation<SInt>> input1 = data1.stream()
-                        .map(BigInteger::valueOf)
-                        .map(NumericBuilder::known)
-                        .collect(Collectors.toList());
-                    List<Computation<SInt>> input2 = data2.stream()
-                        .map(BigInteger::valueOf)
-                        .map(NumericBuilder::known)
-                        .collect(Collectors.toList());
-                    List<Computation<SInt>> input3 = data3.stream()
-                        .map(BigInteger::valueOf)
-                        .map(NumericBuilder::known)
-                        .collect(Collectors.toList());
+          Application<Void, ProtocolBuilderNumeric> app =
+              builder -> {
+                NumericBuilder NumericBuilder = builder.numeric();
+                List<Computation<SInt>> input1 = data1.stream()
+                    .map(BigInteger::valueOf)
+                    .map(NumericBuilder::known)
+                    .collect(Collectors.toList());
+                List<Computation<SInt>> input2 = data2.stream()
+                    .map(BigInteger::valueOf)
+                    .map(NumericBuilder::known)
+                    .collect(Collectors.toList());
+                List<Computation<SInt>> input3 = data3.stream()
+                    .map(BigInteger::valueOf)
+                    .map(NumericBuilder::known)
+                    .collect(Collectors.toList());
 
-                    Computation<SInt> covariance = builder.seq(new Covariance(input1, input2));
-                    Computation<List<List<Computation<SInt>>>> covarianceMatrix = builder
-                        .seq(new CovarianceMatrix(Arrays.asList(input1, input2, input3)));
+                Computation<SInt> covariance = builder.seq(new Covariance(input1, input2));
+                Computation<List<List<Computation<SInt>>>> covarianceMatrix = builder
+                    .seq(new CovarianceMatrix(Arrays.asList(input1, input2, input3)));
 
-                    builder.par((par) -> {
-                      NumericBuilder open = par.numeric();
-                      outputCovariance = open.open(covariance);
-                      List<List<Computation<SInt>>> covarianceMatrixOut = covarianceMatrix.out();
-                      List<List<Computation<BigInteger>>> openCovarianceMatrix = new ArrayList<>(
-                          covarianceMatrixOut.size());
-                      for (List<Computation<SInt>> computations : covarianceMatrixOut) {
-                        List<Computation<BigInteger>> computationList = new ArrayList<>(
-                            computations.size());
-                        openCovarianceMatrix.add(computationList);
-                        for (Computation<SInt> computation : computations) {
-                          computationList.add(open.open(computation));
-                        }
-                      }
-                      outputCovarianceMatix = openCovarianceMatrix;
-                      return null;
-                    });
+                return builder.par((par) -> {
+                  NumericBuilder open = par.numeric();
+                  outputCovariance = open.open(covariance);
+                  List<List<Computation<SInt>>> covarianceMatrixOut = covarianceMatrix.out();
+                  List<List<Computation<BigInteger>>> openCovarianceMatrix = new ArrayList<>(
+                      covarianceMatrixOut.size());
+                  for (List<Computation<SInt>> computations : covarianceMatrixOut) {
+                    List<Computation<BigInteger>> computationList = new ArrayList<>(
+                        computations.size());
+                    openCovarianceMatrix.add(computationList);
+                    for (Computation<SInt> computation : computations) {
+                      computationList.add(open.open(computation));
+                    }
+                  }
+                  outputCovarianceMatix = openCovarianceMatrix;
+                  return () -> null;
+                });
+              };
 
-                  }).build();
-            }
-          };
-          
           secureComputationEngine
-          .runApplication(app, ResourcePoolCreator.createResourcePool(conf.sceConf));
+              .runApplication(app, ResourcePoolCreator.createResourcePool(conf.sceConf));
           BigInteger covariance = outputCovariance.out();
           double sum = 0.0;
           for (int entry : data1) {
@@ -274,7 +256,7 @@ public class StatisticsTests {
             ssd += (entry - mean1Exact) * (entry - mean1Exact);
           }
           double varianceExact = ssd / (data1.size() - 1);
-          
+
           double covarianceExact = 0.0;
           for (int i = 0; i < data1.size(); i++) {
             covarianceExact += (data1.get(i) - mean1Exact) * (data2.get(i) - mean2Exact);
