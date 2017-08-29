a810bf3e36db388a391afc00f297f4ca853edd3a
Peter F. Frandsen <pff@partisia.com>
2017-08-29 16:35:47 +0200

    Dead code removed

--- core/src/main/java/dk/alexandra/fresco/framework/math/Util.java
+++ core/src/main/java/dk/alexandra/fresco/framework/math/Util.java
@@ -28,8 +28,6 @@ package dk.alexandra.fresco.framework.math;
 
 public class Util {
 
-	private static int m[] = {0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80};
-	
 	/**
 	 * Computes the floor(log_2(x))
 	 */
