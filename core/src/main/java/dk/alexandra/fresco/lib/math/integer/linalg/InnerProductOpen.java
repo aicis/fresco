44d4eea8c5d9ab32f00ced43cffe741faabeb037
Peter F. Frandsen <pff@partisia.com>
2017-08-30 14:29:57 +0200

    Reindented code

--- core/src/main/java/dk/alexandra/fresco/lib/math/integer/linalg/InnerProductOpen.java
+++ core/src/main/java/dk/alexandra/fresco/lib/math/integer/linalg/InnerProductOpen.java
@@ -22,18 +22,15 @@ public class InnerProductOpen implements ComputationBuilder<SInt, ProtocolBuilde
 
   @Override
   public Computation<SInt> buildComputation(ProtocolBuilderNumeric builder) {
-    return builder
-        .par(parallel -> {
-          List<Computation<SInt>> result = new ArrayList<>(aVector.size());
-          NumericBuilder numericBuilder = parallel.numeric();
-          for (int i = 0; i < aVector.size(); i++) {
-            BigInteger nextA = aVector.get(i);
-            Computation<SInt> nextB = bVector.get(i);
-            result.add(numericBuilder.mult(nextA, nextB));
-          }
-          return () -> result;
-        })
-        .seq((seq, list) -> seq.advancedNumeric().sum(list)
-        );
+    return builder.par(parallel -> {
+      List<Computation<SInt>> result = new ArrayList<>(aVector.size());
+      NumericBuilder numericBuilder = parallel.numeric();
+      for (int i = 0; i < aVector.size(); i++) {
+        BigInteger nextA = aVector.get(i);
+        Computation<SInt> nextB = bVector.get(i);
+        result.add(numericBuilder.mult(nextA, nextB));
+      }
+      return () -> result;
+    }).seq((seq, list) -> seq.advancedNumeric().sum(list));
   }
 }
