e57bf20464e720e3edd6246272c913ce5c1122cd
Peter F. Frandsen <pff@partisia.com>
2017-09-08 15:34:11 +0200

    Frixed Peters broken commit

---
+++ core/src/main/java/dk/alexandra/fresco/framework/builder/ComputationDirectory.java
@@ -0,0 +1,9 @@
+package dk.alexandra.fresco.framework.builder;
+
+/**
+ * Marker interface for every bit of functionality that users building actual
+ * applications utilises.
+ */
+public interface ComputationDirectory {
+
+}
