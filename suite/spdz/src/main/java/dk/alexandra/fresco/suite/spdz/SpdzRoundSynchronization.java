017f4776dec4cbd1b34c8cd81564aa45d99b266e
damgaard <kasper.damgaard@alexandra.dk>
2017-09-15 10:47:03 +0200

    Forgot the round sync

--- suite/spdz/src/main/java/dk/alexandra/fresco/suite/spdz/SpdzRoundSynchronization.java
+++ suite/spdz/src/main/java/dk/alexandra/fresco/suite/spdz/SpdzRoundSynchronization.java
@@ -4,55 +4,70 @@ import dk.alexandra.fresco.framework.network.SCENetwork;
 import dk.alexandra.fresco.framework.sce.evaluator.BatchedStrategy;
 import dk.alexandra.fresco.framework.sce.evaluator.ProtocolCollectionList;
 import dk.alexandra.fresco.suite.ProtocolSuite.RoundSynchronization;
+import dk.alexandra.fresco.suite.spdz.datatypes.SpdzOutputProtocol;
 import dk.alexandra.fresco.suite.spdz.gates.SpdzMacCheckProtocol;
 import dk.alexandra.fresco.suite.spdz.storage.SpdzStorage;
 import java.io.IOException;
+import java.util.List;
 
 /**
- * A default implementation of the round synchronization for spdz - mostly doing
- * the MAC check if needed.
+ * A default implementation of the round synchronization for spdz - mostly doing the MAC check if
+ * needed.
  */
 public class SpdzRoundSynchronization implements RoundSynchronization<SpdzResourcePool> {
 
   private static final int macCheckThreshold = 100000;
+  private int gatesEvaluated = 0;
 
-  private void MACCheck(SpdzResourcePool resourcePool,
-      SCENetwork sceNetworks) throws IOException {
-
+  private void doMACCheck(SpdzResourcePool resourcePool, SCENetwork sceNetworks)
+      throws IOException {
     SpdzStorage storage = resourcePool.getStore();
-
-    SpdzMacCheckProtocol macCheck = new SpdzMacCheckProtocol(
-        resourcePool.getSecureRandom(),
-        resourcePool.getMessageDigest(),
-        storage,
-        null, resourcePool.getModulus());
-
     int batchSize = 128;
 
-    do {
-      ProtocolCollectionList protocolCollectionList =
-          new ProtocolCollectionList(batchSize);
-      macCheck.getNextProtocols(protocolCollectionList);
+    if (!storage.getOpenedValues().isEmpty()) {
+      SpdzMacCheckProtocol macCheck = new SpdzMacCheckProtocol(resourcePool.getSecureRandom(),
+          resourcePool.getMessageDigest(), storage, null, resourcePool.getModulus());
 
-      BatchedStrategy.processBatch(protocolCollectionList, sceNetworks, 0, resourcePool);
-    } while (macCheck.hasNextProtocols());
+      do {
+        ProtocolCollectionList protocolCollectionList = new ProtocolCollectionList(batchSize);
+        macCheck.getNextProtocols(protocolCollectionList);
 
-    //reset boolean value
-    resourcePool.setOutputProtocolInBatch(false);
+        BatchedStrategy.processBatch(protocolCollectionList, sceNetworks, 0, resourcePool);
+      } while (macCheck.hasNextProtocols());
+    }
+  }
+
+  private void checkMACsInBatch(SpdzResourcePool resourcePool, SCENetwork sceNetworks)
+      throws IOException {
+    doMACCheck(resourcePool, sceNetworks);
+
+    // Check for output protocols in the batch. If any are present, evaluate them one at a time with
+    // a MAC Check in between to prevent cheating.
+    List<SpdzOutputProtocol<?>> outputProtocols = resourcePool.getOutputProtocolsInBatch();
+    for (SpdzOutputProtocol<?> p : outputProtocols) {
+      SpdzBatchedStrategy.processBatch(p, sceNetworks, resourcePool, 1);
+
+      // Run a MAC Check after each output protocol to ensure validity
+      doMACCheck(resourcePool, sceNetworks);
+    }
+
+    // reset stuff to gain memory back and not check them again.
+    resourcePool.getOutputProtocolsInBatch().clear();
+    this.gatesEvaluated = 0;
   }
 
   @Override
   public void finishedEval(SpdzResourcePool resourcePool, SCENetwork sceNetwork)
       throws IOException {
-    MACCheck(resourcePool, sceNetwork);
+    checkMACsInBatch(resourcePool, sceNetwork);
   }
 
   @Override
   public void finishedBatch(int gatesEvaluated, SpdzResourcePool resourcePool,
       SCENetwork sceNetwork) throws IOException {
-    gatesEvaluated += gatesEvaluated;
-    if (gatesEvaluated > macCheckThreshold || resourcePool.isOutputProtocolInBatch()) {
-      MACCheck(resourcePool, sceNetwork);
+    this.gatesEvaluated += gatesEvaluated;
+    if (this.gatesEvaluated > macCheckThreshold || resourcePool.isOutputProtocolInBatch()) {
+      checkMACsInBatch(resourcePool, sceNetwork);
     }
   }
 }
