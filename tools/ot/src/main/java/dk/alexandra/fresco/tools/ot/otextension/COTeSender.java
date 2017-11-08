483d3d33678eff2f94001cd826bfd5ac9957f463
jot2re <33345641+jot2re@users.noreply.github.com>
2017-11-08 17:12:20 +0100

    Started draft for COTe

--- tools/ot/src/main/java/dk/alexandra/fresco/tools/ot/otextension/COTeSender.java
+++ tools/ot/src/main/java/dk/alexandra/fresco/tools/ot/otextension/COTeSender.java
@@ -18,14 +18,15 @@ import dk.alexandra.fresco.framework.util.Pair;
  */
 public class COTeSender extends COTeShared {
 
-  // Random messages used for the seed OTs
-  private List<Pair<BigInteger, BigInteger>> seeds;
-  private List<Pair<SecureRandom, SecureRandom>> prgs;
+  // The prgs based on the seeds learned from OT
+  private List<SecureRandom> prgs;
+  // The random messages choices for the random seed OTs
+  private List<Boolean> otChoices;
 
   public COTeSender(int otherID, int kBitLength, int lambdaSecurityParam,
       Random rand, Network network) {
     super(otherID, kBitLength, lambdaSecurityParam, rand, network);
-    this.seeds = new ArrayList<>(kBitLength);
+    this.otChoices = new ArrayList<>(kBitLength);
     this.prgs = new ArrayList<>(kBitLength);
   }
 
@@ -37,24 +38,25 @@ public class COTeSender extends COTeShared {
     if (initialized) {
       throw new IllegalStateException("Already initialized");
     }
+    // Complete the seed OTs acting as the receiver (NOT the sender)
     for (int i = 0; i < kBitLength; i++) {
-      BigInteger seedZero = new BigInteger(kBitLength, rand);
-      BigInteger seedFirst = new BigInteger(kBitLength, rand);
-//      ot.send(seedZero, seedFirst);
-      seeds.add(new Pair<>(seedZero, seedFirst));
+      Boolean choiceBit = rand.nextBoolean();
+      otChoices.add(choiceBit);
+      BigInteger message = ot.receive(choiceBit);
       // Initialize the PRGs with the random messages
-      SecureRandom prgZero = new SecureRandom(seedZero.toByteArray());
-      SecureRandom prgFirst = new SecureRandom(seedFirst.toByteArray());
-      prgs.add(new Pair<>(prgZero, prgFirst));
+      SecureRandom prg = new SecureRandom(message.toByteArray());
+      prgs.add(prg);
     }
     initialized = true;
   }
 
   /**
-   * Constructs a new batch of correlated OTs with errors. 
-   * @param size Amount of OTs to construct
+   * Constructs a new batch of correlated OTs with errors.
+   * 
+   * @param size
+   *          Amount of OTs to construct
    */
-  public void extend(int size) {
+  public List<Pair<byte[], byte[]>> extend(int size) {
     if (size < 1) {
       throw new IllegalArgumentException(
           "The amount of OTs must be a positive integer");
@@ -65,11 +67,28 @@ public class COTeSender extends COTeShared {
     // Compute how many bytes we need for "size" OTs by dividing "size" by 8
     // (the amount of bits in the primitive type; byte), rounding up
     int bytesNeeded = (size + 8 - 1) / 8;
-    // Use a prg to expand the seeds
-    List<Pair<byte[], byte[]>> tVectors = new ArrayList<>(kBitLength);
+    List<byte[]> tVec = new ArrayList<>(kBitLength);
     for (int i = 0; i < kBitLength; i++) {
-//      prgs.get(i).getFirst().nextBytes(size);
+      // Expand the message learned from the seed OTs using a PRG
+      byte[] tVal = new byte[bytesNeeded];
+      prgs.get(i).nextBytes(tVal);
+      tVec.add(tVal);
     }
-    
+    List<byte[]> uVec = receiveList(kBitLength);
+    List<byte[]> qVec = new ArrayList<>(kBitLength);
+    // Compute the q vector based on the random choices from the seed OTs, i.e
+    // qVec = otChoices AND uVec XOR tVec
+    for (int i = 0; i < kBitLength; i++) {
+      byte[] currentArr;
+      if (otChoices.get(i) == true)
+        currentArr = xor(uVec.get(i), tVec.get(i));
+      else
+        currentArr = tVec.get(i);
+      qVec.add(currentArr);
+    }
+    // Complete tilt-your-head by transposing the message "matrix"
+    List<Pair<byte[], byte[]>> messages = new ArrayList<>(size);
+    // TODO
+    return messages;
   }
 }
--- tools/ot/src/main/java/dk/alexandra/fresco/tools/ot/otextension/COTeShared.java
+++ tools/ot/src/main/java/dk/alexandra/fresco/tools/ot/otextension/COTeShared.java
@@ -1,11 +1,14 @@
 package dk.alexandra.fresco.tools.ot.otextension;
 
+import java.io.IOException;
 import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 import dk.alexandra.fresco.framework.network.Network;
-import dk.alexandra.fresco.tools.ot.base.DummyOTBatch;
-import dk.alexandra.fresco.tools.ot.base.OTBatch;
+import dk.alexandra.fresco.tools.ot.base.DummyOT;
+import dk.alexandra.fresco.tools.ot.base.OT;
 
 /**
  * Superclass containing the common variables and methods 
@@ -22,7 +25,7 @@ public class COTeShared {
   protected Network network;
   // Internal state variables
   protected boolean initialized;
-  protected OTBatch<BigInteger> ot;
+  protected OT<BigInteger> ot;
 
   public COTeShared(int otherID, int kBitLength, int lambdaSecurityParam, Random rand, 
       Network network) {
@@ -34,7 +37,7 @@ public class COTeShared {
     this.kBitLength = kBitLength;
     this.lambdaSecurityParam = lambdaSecurityParam;
     this.rand = rand;
-    this.ot = new DummyOTBatch(otherID, network);
+    this.ot = new DummyOT(otherID, network);
     this.network = network;
   }
 
@@ -78,4 +81,90 @@ public class COTeShared {
     this.network = network;
   }
 
+  /**
+   * Computes the XOR of each element in a list of byte arrays. If the lists are
+   * not of equal length or any of the byte arrays are not of equal size, then
+   * an IllegalArgument exception is thrown
+   * 
+   * @param vector1
+   *          First input list
+   * @param vector2
+   *          Second input list
+   * @return A new list which is the XOR of the two input lists
+   */
+  protected List<byte[]> xor(List<byte[]> vector1, List<byte[]> vector2) {
+    if (vector1.size() != vector2.size())
+      throw new IllegalArgumentException("The vectors are not of equal length");
+    List<byte[]> res = new ArrayList<>(vector1.size());
+    for (int i = 0; i < vector1.size(); i++) {
+      byte[] currentArr = xor(vector1.get(i), vector2.get(i));
+      res.add(currentArr);
+    }
+    return res;
+  }
+
+  /**
+   * Computes the XOR of each element in a byte array. If the byte arrays are
+   * not of equal size, then an IllegalArgument exception is thrown
+   * 
+   * @param arr1
+   *          First byte array
+   * @param arr2
+   *          Second byte array
+   * @return A new byte array which is the XOR of the two input arrays
+   */
+  protected byte[] xor(byte[] arr1, byte[] arr2) {
+    int bytesNeeded = arr1.length;
+    if (bytesNeeded != arr2.length)
+      throw new IllegalArgumentException(
+          "The byte arrays are not of equal lengh");
+    byte[] res = new byte[bytesNeeded];
+    for (int i = 0; i < bytesNeeded; i++) {
+      // Compute the XOR (addition in GF2) of arr1 and arr2
+      res[i] = (byte) (arr1[i] ^ arr2[i]);
+    }
+    return res;
+  }
+
+  /**
+   * Sends a list of byte arrays to the default (0) channel
+   * 
+   * @param vector
+   *          Vector to send
+   * @return Returns true if the transmission was successful
+   */
+  protected boolean sendList(List<byte[]> vector) {
+    try {
+      for (byte[] currentArr : vector) {
+        network.send(0, otherID, currentArr);
+      }
+    } catch (IOException e) {
+      System.out.println("Broke while sending " + e);
+      return false;
+    }
+    return true;
+  }
+
+  /**
+   * Receives a list of byte arrays from the default (0) channel
+   * 
+   * @param vector
+   *          Vector to receive
+   * @param size
+   *          Amount of elements in vector to receive
+   * @return The list of received elements, or null in case an error occurred.
+   */
+  protected List<byte[]> receiveList(int size) {
+    List<byte[]> vector = new ArrayList<>(size);
+    try {
+      for (int i = 0; i < size; i++) {
+        byte[] currentArr = network.receive(0, otherID);
+        vector.add(currentArr);
+      }
+    } catch (IOException e) {
+      System.out.println("Broke while sending " + e);
+      return null;
+    }
+    return vector;
+  }
 }
