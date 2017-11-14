3554e64c743ea64289c9ae68eb04e8435c25bc68
Michael Stausholm <michael.stausholm@alexandra.dk>
2017-11-14 12:02:00 +0100

    Removed empty (commented) classes + unignored AES test.

--- core/src/test/java/dk/alexandra/fresco/framework/crypto/TestAES.java
+++ core/src/test/java/dk/alexandra/fresco/framework/crypto/TestAES.java
@@ -2,17 +2,16 @@ package dk.alexandra.fresco.framework.crypto;
 
 import java.io.IOException;
 import java.util.Random;
+
 import org.junit.Assert;
-import org.junit.Ignore;
 import org.junit.Test;
 
 public class TestAES {
 
 	@Test
-	@Ignore // TODO[PSN]
-	public void testAes() throws IOException {
+	public void testEncryptAndDecrypt() throws IOException {
 		AES aes = new AES("89SHJsdhu3h4jbd82h", 1024, new Random());
-		byte[] data = new byte[] {0x02, 0x04, 0x10};
+		byte[] data = new byte[] {0x02, 0x04, 0x10, 0x77, 0x45, 0x00};
 		byte[] enc = aes.encrypt(data);
 		byte[] dec = aes.decrypt(enc);
 		Assert.assertArrayEquals(data, dec);
@@ -21,4 +20,7 @@ public class TestAES {
 		dec = aes2.decrypt(enc);
 		Assert.assertArrayEquals(data, dec);
 	}
+	
+	
+	
 }
