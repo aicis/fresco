767c231f05a0c1f4978101a1d496b82933bf4584
Peter F. Frandsen <pff@partisia.com>
2017-11-17 21:45:02 +0100

    Constructor test

---
+++ core/src/test/java/dk/alexandra/fresco/lib/crypto/mimc/MiMCConstantsTest.java
@@ -0,0 +1,11 @@
+package dk.alexandra.fresco.lib.crypto.mimc;
+
+import org.junit.Test;
+
+public class MiMCConstantsTest {
+
+  @Test
+  public void constructor() throws Exception {
+    new MiMCConstants();
+  }
+}
