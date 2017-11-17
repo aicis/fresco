f09e58200c81e54e089acc323da22477cff04ae4
Peter F. Frandsen <pff@partisia.com>
2017-11-17 21:19:47 +0100

    Rewrote by making state immutable and going from linked list to array list

--- core/src/main/java/dk/alexandra/fresco/framework/builder/numeric/DefaultPreprocessedValues.java
+++ core/src/main/java/dk/alexandra/fresco/framework/builder/numeric/DefaultPreprocessedValues.java
@@ -2,57 +2,51 @@ package dk.alexandra.fresco.framework.builder.numeric;
 
 import dk.alexandra.fresco.framework.DRes;
 import dk.alexandra.fresco.framework.value.SInt;
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Default implementation which should only be used if for some reason preprocessing is not
  * possible.
- * 
  */
 public class DefaultPreprocessedValues implements PreprocessedValues {
 
   private ProtocolBuilderNumeric builder;
-  
+
   public DefaultPreprocessedValues(ProtocolBuilderNumeric builder) {
     this.builder = builder;
   }
-  
+
   @Override
   public DRes<List<DRes<SInt>>> getExponentiationPipe(int pipeLength) {
     return builder.seq(b -> {
       DRes<SInt> r = b.numeric().randomElement();
       DRes<SInt> inverse = b.advancedNumeric().invert(r);
-      LinkedList<DRes<SInt>> list = new LinkedList<>();
+      ArrayList<DRes<SInt>> list = new ArrayList<>();
       list.add(inverse);
       list.add(r);
       return () -> new IterationState(2, list);
     }).whileLoop(
-      (state) -> state.round <= pipeLength + 1,
-      (seq, state) -> {      
-        DRes<SInt> r = state.value.get(1);
-        DRes<SInt> last = state.value.getLast();
-        state.value.add(seq.numeric().mult(last, r));
-        state.round++;
-        return state;
-      }).seq((seq, state) -> {
-        return () -> state.value;
-      });
+        (state) -> state.round <= pipeLength + 1,
+        (seq, state) -> {
+          DRes<SInt> r = state.value.get(1);
+          DRes<SInt> last = state.value.get(state.value.size() - 1);
+          List<DRes<SInt>> values = new ArrayList<>(state.value);
+          values.add(seq.numeric().mult(last, r));
+          return () -> new IterationState(state.round + 1, values);
+        }).seq((seq, state) -> {
+      return () -> state.value;
+    });
   }
 
-  private static final class IterationState implements DRes<IterationState> {
+  private static final class IterationState {
 
-    private int round;
-    private final LinkedList<DRes<SInt>> value;
+    private final int round;
+    private final List<DRes<SInt>> value;
 
-    private IterationState(int round, LinkedList<DRes<SInt>> value) {
+    private IterationState(int round, List<DRes<SInt>> value) {
       this.round = round;
       this.value = value;
-    }
-
-    @Override
-    public IterationState out() {
-      return this;
     }
   }
 }
