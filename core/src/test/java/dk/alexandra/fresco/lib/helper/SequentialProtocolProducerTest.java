aec1ad4f446927d6f1d41700645fb7a84bd1b2ac
Peter F. Frandsen <pff@partisia.com>
2017-11-17 22:18:16 +0100

    Test of ToString

---
+++ core/src/test/java/dk/alexandra/fresco/lib/helper/SequentialProtocolProducerTest.java
@@ -0,0 +1,21 @@
+package dk.alexandra.fresco.lib.helper;
+
+import dk.alexandra.fresco.suite.dummy.bool.DummyBooleanNotProtocol;
+import org.hamcrest.core.Is;
+import org.hamcrest.core.StringContains;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SequentialProtocolProducerTest {
+
+  @Test
+  public void testToString() throws Exception {
+    SequentialProtocolProducer sequentialProtocolProducer = new SequentialProtocolProducer();
+    sequentialProtocolProducer
+        .append(new SingleProtocolProducer<>(new DummyBooleanNotProtocol(null)));
+    String toString = sequentialProtocolProducer.toString();
+    Assert.assertThat(toString, StringContains.containsString("1"));
+    Assert.assertThat(sequentialProtocolProducer.toString(), Is.is(toString));
+  }
+
+}
