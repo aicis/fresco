41b73fe03848d11c3268f942dc2413fb3d873bac
Nikolaj Volgushev <n1v0lg@users.noreply.github.com>
2017-11-20 10:40:06 +0100

    Fixed unit tests

--- tools/ot/src/test/java/dk/alexandra/fresco/tools/ot/otextension/TestCote.java
+++ tools/ot/src/test/java/dk/alexandra/fresco/tools/ot/otextension/TestCote.java
@@ -3,11 +3,16 @@ package dk.alexandra.fresco.tools.ot.otextension;
 import static org.junit.Assert.assertEquals;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
 
 import org.junit.Test;
 
+import dk.alexandra.fresco.framework.Party;
+import dk.alexandra.fresco.framework.configuration.NetworkConfiguration;
+import dk.alexandra.fresco.framework.configuration.NetworkConfigurationImpl;
 import dk.alexandra.fresco.framework.network.KryoNetNetwork;
 import dk.alexandra.fresco.framework.network.Network;
 
@@ -16,9 +21,9 @@ public class TestCote {
   /**** POSITIVE TESTS. ****/
   @Test
   public void testGetBit() {
-    byte[] byteArray = new byte[] { (byte) 0x54, (byte) 0x04 };
+    byte[] byteArray = new byte[] {(byte) 0x54, (byte) 0x04};
     assertEquals(true, CoteSender.getBit(byteArray, 13));
-    byteArray = new byte[] { (byte) 0xFF, (byte) 0xFB };
+    byteArray = new byte[] {(byte) 0xFF, (byte) 0xFB};
     assertEquals(false, CoteSender.getBit(byteArray, 13));
   }
 
@@ -26,7 +31,21 @@ public class TestCote {
   @Test
   public void testIllegalInit() {
     Random rand = new Random();
-    Network network = new KryoNetNetwork();
+    // fake network
+    Network network = new Network() {
+      @Override
+      public void send(int partyId, byte[] data) {}
+
+      @Override
+      public byte[] receive(int partyId) {
+        return null;
+      }
+
+      @Override
+      public int getNoOfParties() {
+        return 0;
+      }
+    };
     CoteSender badSender;
     CoteReceiver badReceiver;
     boolean thrown = false;
@@ -64,13 +83,12 @@ public class TestCote {
     try {
       badSender = new CoteSender(0, 127, 80, rand, network);
     } catch (IllegalArgumentException e) {
-      assertEquals("Computational security parameter must be divisible by 8",
-          e.getMessage());
+      assertEquals("Computational security parameter must be divisible by 8", e.getMessage());
       thrown = true;
     }
     assertEquals(thrown, true);
   }
-  
+
   @Test
   public void testIllegalXor() {
     byte[] arr1 = new byte[34];
