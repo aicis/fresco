bc3857e69de1537630601638cd3fecdbcb611ad4
Peter F. Frandsen <pff@partisia.com>
2017-11-20 10:11:50 +0100

    Fixed coverage on PArty, secretSharedKey can be null

--- scapiNetwork/src/test/java/dk/alexandra/fresco/network/TestScapiNetwork.java
+++ scapiNetwork/src/test/java/dk/alexandra/fresco/network/TestScapiNetwork.java
@@ -74,8 +74,7 @@ public class TestScapiNetwork {
       Map<Integer, Party> partyMap = new HashMap<>();
       int id = 1;
       for (int port : ports) {
-        Party party = new Party(id, "localhost", port);
-        party.setSecretSharedKey("MDEyMzQ1Njc4OWFiY2RlZg==");
+        Party party = new Party(id, "localhost", port, "MDEyMzQ1Njc4OWFiY2RlZg==");
         partyMap.put(id, party);
         id++;
       }
