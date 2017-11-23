112935dafd88832101f4261da29aa6c4d0eebfbb
Nikolaj Volgushev <n1v0lg@users.noreply.github.com>
2017-11-23 13:37:59 +0100

    Random byte gen

--- tools/ot/src/main/java/dk/alexandra/fresco/tools/ot/otextension/Helper.java
+++ tools/ot/src/main/java/dk/alexandra/fresco/tools/ot/otextension/Helper.java
@@ -9,18 +9,16 @@ import java.io.ObjectOutput;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.util.List;
+import java.util.Random;
 
 public class Helper {
+  
   /**
-   * Returns the "bit" number bit, reading from left-to-right, from a byte
-   * array.
+   * Returns the "bit" number bit, reading from left-to-right, from a byte array.
    * 
-   * @param input
-   *          The arrays of which to retrieve a bit
-   * @param bit
-   *          The index of the bit, counting from 0
-   * @return Returns the "bit" number bit, reading from left-to-right, from
-   *         "input"
+   * @param input The arrays of which to retrieve a bit
+   * @param bit The index of the bit, counting from 0
+   * @return Returns the "bit" number bit, reading from left-to-right, from "input"
    */
   public static boolean getBit(byte[] input, int bit) {
     if (bit < 0) {
@@ -37,19 +35,15 @@ public class Helper {
   }
 
   /**
-   * Computes the XOR of each element in a list of byte arrays. This is done
-   * in-place in "vector1". If the lists are not of equal length or any of the
-   * byte arrays are not of equal size, then an IllegalArgument exception is
-   * thrown
+   * Computes the XOR of each element in a list of byte arrays. This is done in-place in "vector1".
+   * If the lists are not of equal length or any of the byte arrays are not of equal size, then an
+   * IllegalArgument exception is thrown
    * 
-   * @param vector1
-   *          First input list
-   * @param vector2
-   *          Second input list
+   * @param vector1 First input list
+   * @param vector2 Second input list
    * @return A new list which is the XOR of the two input lists
    */
-  public static void xor(List<byte[]> vector1,
-      List<byte[]> vector2) {
+  public static void xor(List<byte[]> vector1, List<byte[]> vector2) {
     if (vector1.size() != vector2.size()) {
       throw new IllegalArgumentException("The vectors are not of equal length");
     }
@@ -59,28 +53,24 @@ public class Helper {
   }
 
   /**
-   * Computes the XOR of each element in a byte array. This is done in-place in
-   * "arr1". If the byte arrays are not of equal size, then an IllegalArgument
-   * exception is thrown
+   * Computes the XOR of each element in a byte array. This is done in-place in "arr1". If the byte
+   * arrays are not of equal size, then an IllegalArgument exception is thrown
    * 
-   * @param arr1
-   *          First byte array
-   * @param arr2
-   *          Second byte array
+   * @param arr1 First byte array
+   * @param arr2 Second byte array
    * @return A new byte array which is the XOR of the two input arrays
    */
   public static void xor(byte[] arr1, byte[] arr2) {
     int bytesNeeded = arr1.length;
     if (bytesNeeded != arr2.length) {
-      throw new IllegalArgumentException(
-          "The byte arrays are not of equal length");
+      throw new IllegalArgumentException("The byte arrays are not of equal length");
     }
     for (int i = 0; i < bytesNeeded; i++) {
       // Compute the XOR (addition in GF2) of arr1 and arr2
       arr1[i] ^= arr2[i];
     }
   }
-
+  
   /**
    * Serialize a serializable value
    * 
@@ -126,5 +116,11 @@ public class Helper {
       bis.close();
       in.close();
     }
+  }
+  
+  public static byte[] randomByteArray(int size, Random rand) {
+    byte[] array = new byte[size];
+    rand.nextBytes(array);
+    return array;
   }
 }
