96901e2919cec070d9a57b7ceb937ef7b7e65af8
Peter Sebastian Nordholt <peter.s.nordholt@alexandra.dk>
2017-11-28 11:01:03 +0100

    Test for the SecureComputationEngineImpl class.

---
+++ core/src/test/java/dk/alexandra/fresco/framework/sce/TestSecureComputationEngineImpl.java
@@ -0,0 +1,105 @@
+package dk.alexandra.fresco.framework.sce;
+
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
+
+import dk.alexandra.fresco.framework.Application;
+import dk.alexandra.fresco.framework.DRes;
+import dk.alexandra.fresco.framework.ProtocolEvaluator;
+import dk.alexandra.fresco.framework.builder.numeric.ProtocolBuilderNumeric;
+import dk.alexandra.fresco.framework.sce.evaluator.BatchedProtocolEvaluator;
+import dk.alexandra.fresco.framework.sce.evaluator.SequentialStrategy;
+import dk.alexandra.fresco.framework.value.SInt;
+import dk.alexandra.fresco.suite.dummy.arithmetic.DummyArithmeticProtocolSuite;
+import dk.alexandra.fresco.suite.dummy.arithmetic.DummyArithmeticResourcePool;
+import dk.alexandra.fresco.suite.dummy.arithmetic.DummyArithmeticResourcePoolImpl;
+import java.math.BigInteger;
+import java.security.SecureRandom;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestSecureComputationEngineImpl {
+
+  private SecureComputationEngineImpl<DummyArithmeticResourcePool, ProtocolBuilderNumeric> sce;
+
+  @Before
+  public void setup() {
+    DummyArithmeticProtocolSuite suite =
+        new DummyArithmeticProtocolSuite(BigInteger.valueOf(101), 2);
+    ProtocolEvaluator<DummyArithmeticResourcePool, ProtocolBuilderNumeric> eval =
+        new BatchedProtocolEvaluator<>(new SequentialStrategy<>(), suite);
+    sce = new SecureComputationEngineImpl<>(suite, eval);
+    sce.shutdownSCE(); // test this before setup
+    sce.setup();
+  }
+
+  @Test
+  public void testSecureComputationEngineImpl() {
+    assertThat(sce, instanceOf(SecureComputationEngineImpl.class));
+  }
+
+  @Test
+  public void testRunApplication() {
+    Application<BigInteger, ProtocolBuilderNumeric> app =
+        new Application<BigInteger, ProtocolBuilderNumeric>() {
+
+          @Override
+          public DRes<BigInteger> buildComputation(ProtocolBuilderNumeric builder) {
+            DRes<SInt> a = builder.numeric().known(BigInteger.valueOf(10));
+            DRes<SInt> b = builder.numeric().known(BigInteger.valueOf(10));
+            return builder.numeric().open(builder.numeric().add(a, b));
+          }
+        };
+    DummyArithmeticResourcePool rp = new DummyArithmeticResourcePoolImpl(0, 1, new Random(),
+        new SecureRandom(), BigInteger.valueOf(101));
+
+    BigInteger b = sce.runApplication(app, rp, null);
+    assertThat(b, is(BigInteger.valueOf(20)));
+  }
+
+  @Test(expected = RuntimeException.class)
+  public void testRunApplicationAppThrows() {
+    Application<Object, ProtocolBuilderNumeric> app =
+        new Application<Object, ProtocolBuilderNumeric>() {
+
+          @Override
+          public DRes<Object> buildComputation(ProtocolBuilderNumeric builder) {
+            throw new RuntimeException();
+          }
+        };
+    DummyArithmeticResourcePool rp = new DummyArithmeticResourcePoolImpl(0, 1, new Random(),
+        new SecureRandom(), BigInteger.valueOf(101));
+    sce.runApplication(app, rp, null);
+    fail("Should not be reachable");
+  }
+
+  @Test(expected = RuntimeException.class)
+  public void testRunApplicationAppTimesOut() {
+    Application<Object, ProtocolBuilderNumeric> app =
+        new Application<Object, ProtocolBuilderNumeric>() {
+
+          @Override
+          public DRes<Object> buildComputation(ProtocolBuilderNumeric builder) {
+            while (true) {
+
+            }
+          }
+        };
+    DummyArithmeticResourcePool rp = new DummyArithmeticResourcePoolImpl(0, 1, new Random(),
+        new SecureRandom(), BigInteger.valueOf(101));
+    sce.setRunTimeout(1, TimeUnit.NANOSECONDS);
+    sce.runApplication(app, rp, null);
+    fail("Should not be reachable");
+  }
+
+  @After
+  public void tearDown() {
+    sce.shutdownSCE();
+  }
+
+}
