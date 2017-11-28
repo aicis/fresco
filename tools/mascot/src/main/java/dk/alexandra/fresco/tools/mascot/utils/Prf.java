f8737cde4e294d99a5e8c06ae41929ca74a5490f
Nikolaj Volgushev <n1v0lg@users.noreply.github.com>
2017-11-28 14:35:51 +0100

    Prf interface comments

--- tools/mascot/src/main/java/dk/alexandra/fresco/tools/mascot/utils/Prf.java
+++ tools/mascot/src/main/java/dk/alexandra/fresco/tools/mascot/utils/Prf.java
@@ -7,7 +7,16 @@ import dk.alexandra.fresco.tools.mascot.field.FieldElement;
 
 public interface Prf {
 
-  public FieldElement evaluate(StrictBitVector seed, BigInteger counter, BigInteger modulus,
+  /**
+   * Deterministically generates random field element using the base seed and counter as a combined seed.
+   * 
+   * @param baseSeed makes up seed to prf together with counter
+   * @param counter makes up seed to prf together with base seed
+   * @param modulus prime modulus defining the field
+   * @param bitLength the bit length of the prime modulus
+   * @return random field element
+   */
+  public FieldElement evaluate(StrictBitVector baseSeed, BigInteger counter, BigInteger modulus,
       int bitLength);
 
 }
