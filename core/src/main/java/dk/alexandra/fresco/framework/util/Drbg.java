f9660b9ceabbd03a3e9ab4d6fa43c078bc35e679
Peter Sebastian Nordholt <peter.s.nordholt@alexandra.dk>
2017-12-06 21:34:13 +0100

    Small update to java doc.

--- core/src/main/java/dk/alexandra/fresco/framework/util/Drbg.java
+++ core/src/main/java/dk/alexandra/fresco/framework/util/Drbg.java
@@ -1,16 +1,18 @@
 package dk.alexandra.fresco.framework.util;
 
 /**
+ * Interface for Determenistic Random Bit Generators.
  * <p>
- * Often, in our protocols, we need a source of randomness that can be seeded 'deterministically',
- * e.g. in the sense that two instances created with the same seed yields the same sequence of
- * random bytes, but can still not be easily guessed by an adversary.
+ * Often, we need a source of randomness that
+ * can be seeded 'deterministically', e.g. in the sense that two instances created with the same
+ * seed yields the same sequence of pseudo random bytes, but can still not be easily guessed by an
+ * adversary.
  * </p>
  * <p>
- * Implementations of this class will have the property that data generated is pseudo-random and if
- * all parties uses the same seed(s), calls to {@link #nextBytes(byte[])} will be deterministic and
- * secure against third parties trying to guess the resulting randomness. For actual security
- * guarantees, we refer to the individual implementations.
+ * Implementations of this class will have the property that data generated is
+ * pseudo-random and if all parties uses the same seed(s), calls to {@link #nextBytes(byte[])} will
+ * be deterministic and secure against third parties trying to guess the resulting randomness. For
+ * actual security guarantees, we refer to the individual implementations.
  * </p>
  */
 public interface Drbg {
