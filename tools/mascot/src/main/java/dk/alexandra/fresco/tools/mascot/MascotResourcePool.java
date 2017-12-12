77541a66d675d877b6676e3a7652d95d90310ae8
Nikolaj Volgushev <n1v0lg@users.noreply.github.com>
2017-12-12 15:55:52 +0100

    Mascot resource pool

---
+++ tools/mascot/src/main/java/dk/alexandra/fresco/tools/mascot/MascotResourcePool.java
@@ -0,0 +1,73 @@
+package dk.alexandra.fresco.tools.mascot;
+
+import java.util.List;
+
+import dk.alexandra.fresco.framework.builder.numeric.NumericResourcePool;
+import dk.alexandra.fresco.framework.network.serializers.StrictBitVectorSerializer;
+import dk.alexandra.fresco.tools.commitment.Commitment;
+import dk.alexandra.fresco.tools.commitment.CommitmentSerializer;
+import dk.alexandra.fresco.tools.mascot.field.FieldElement;
+import dk.alexandra.fresco.tools.mascot.field.FieldElementSerializer;
+import dk.alexandra.fresco.tools.mascot.utils.FieldElementPrg;
+
+public interface MascotResourcePool extends NumericResourcePool {
+
+  /**
+   * Gets the party ids.
+   * 
+   * @return party ids
+   */
+  List<Integer> getPartyIds();
+
+  /**
+   * Gets bit length of modulus (k security param in Mascot paper).
+   * 
+   * @return modulus bit length
+   */
+  int getModBitLength();
+
+  /**
+   * Gets OT security parameter num bits (lambda in Mascot paper)
+   * 
+   * @return lambda security parameter
+   */
+  int getLambdaSecurityParam();
+
+  /**
+   * Gets number of factors that go into sacrifice step. <br>
+   * For each triple we generate, we will generate and numLeftFactors - 1 triples for a single right
+   * factor and sacrifice these to authenticate the triple.
+   * 
+   * @return number of factors
+   */
+  int getNumLeftFactors();
+
+  /**
+   * Gets PRG for generating random field elements locally (for this party only).
+   * 
+   * @return PRG
+   */
+  FieldElementPrg getLocalSampler();
+
+  /**
+   * Serializer for {@link FieldElement}.
+   * 
+   * @return serializer
+   */
+  FieldElementSerializer getFieldElementSerializer();
+
+  /**
+   * Serializer for {@link StrictBitVector}
+   * 
+   * @return serializer
+   */
+  StrictBitVectorSerializer getStrictBitVectorSerializer();
+
+  /**
+   * Serializer for {@link Commitment}
+   * 
+   * @return serializer
+   */
+  CommitmentSerializer getCommitmentSerializer();
+
+}
---
+++ tools/mascot/src/main/java/dk/alexandra/fresco/tools/mascot/MascotResourcePoolImpl.java
@@ -0,0 +1,128 @@
+package dk.alexandra.fresco.tools.mascot;
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.List;
+
+import dk.alexandra.fresco.framework.network.serializers.BigIntegerSerializer;
+import dk.alexandra.fresco.framework.network.serializers.StrictBitVectorSerializer;
+import dk.alexandra.fresco.framework.util.Drbg;
+import dk.alexandra.fresco.framework.util.StrictBitVector;
+import dk.alexandra.fresco.tools.commitment.CommitmentSerializer;
+import dk.alexandra.fresco.tools.mascot.field.FieldElementSerializer;
+import dk.alexandra.fresco.tools.mascot.utils.FieldElementPrg;
+import dk.alexandra.fresco.tools.mascot.utils.PaddingPrg;
+
+public class MascotResourcePoolImpl implements MascotResourcePool {
+
+  Integer myId;
+  List<Integer> partyIds;
+  BigInteger modulus;
+  int modBitLength;
+  int lambdaSecurityParam;
+  int numLeftFactors;
+  FieldElementPrg localSampler;
+  FieldElementSerializer fieldElementSerializer;
+  StrictBitVectorSerializer strictBitVectorSerializer;
+  CommitmentSerializer commitmentSerializer;
+  MessageDigest messageDigest;
+
+  public MascotResourcePoolImpl(Integer myId, List<Integer> partyIds, BigInteger modulus,
+      int modBitLength, int lambdaSecurityParam, int prgSeedLength, int numLeftFactors) {
+    super();
+    this.myId = myId;
+    this.partyIds = partyIds;
+    this.modulus = modulus;
+    this.modBitLength = modBitLength;
+    this.lambdaSecurityParam = lambdaSecurityParam;
+    this.numLeftFactors = numLeftFactors;
+    this.localSampler = new PaddingPrg(new StrictBitVector(prgSeedLength, new SecureRandom()));
+    this.fieldElementSerializer = new FieldElementSerializer(modulus, modBitLength);
+    this.strictBitVectorSerializer = new StrictBitVectorSerializer();
+    this.commitmentSerializer = new CommitmentSerializer();
+    try {
+      this.messageDigest = MessageDigest.getInstance("SHA-256");
+    } catch (NoSuchAlgorithmException e) {
+      throw new RuntimeException(e);
+    }
+
+  }
+
+  @Override
+  public List<Integer> getPartyIds() {
+    return partyIds;
+  }
+
+  @Override
+  public BigInteger getModulus() {
+    return modulus;
+  }
+
+  @Override
+  public int getModBitLength() {
+    return modBitLength;
+  }
+
+  @Override
+  public int getLambdaSecurityParam() {
+    return lambdaSecurityParam;
+  }
+
+  @Override
+  public int getNumLeftFactors() {
+    return numLeftFactors;
+  }
+
+  @Override
+  public FieldElementPrg getLocalSampler() {
+    return localSampler;
+  }
+
+  @Override
+  public FieldElementSerializer getFieldElementSerializer() {
+    return fieldElementSerializer;
+  }
+
+  @Override
+  public StrictBitVectorSerializer getStrictBitVectorSerializer() {
+    return strictBitVectorSerializer;
+  }
+
+  @Override
+  public CommitmentSerializer getCommitmentSerializer() {
+    return commitmentSerializer;
+  }
+
+  @Override
+  public BigIntegerSerializer getSerializer() {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public MessageDigest getMessageDigest() {
+    return messageDigest;
+  }
+
+  @Override
+  public BigInteger convertRepresentation(BigInteger b) {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public int getMyId() {
+    return myId;
+  }
+
+  @Override
+  public int getNoOfParties() {
+    return partyIds.size();
+  }
+
+  @Override
+  public Drbg getRandomGenerator() {
+    throw new UnsupportedOperationException();
+  }
+
+}
