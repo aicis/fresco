b6c07dd69e38581e5da48e8687831f257824b533
jot2re <33345641+jot2re@users.noreply.github.com>
2017-12-14 13:06:38 +0100

    Fixed a bug in commitments

--- tools/ot/src/main/java/dk/alexandra/fresco/tools/commitment/Commitment.java
+++ tools/ot/src/main/java/dk/alexandra/fresco/tools/commitment/Commitment.java
@@ -7,16 +7,11 @@ import dk.alexandra.fresco.framework.util.ExceptionConverter;
 import java.security.MessageDigest;
 import java.util.Arrays;
 
-import dk.alexandra.fresco.framework.MaliciousException;
-import dk.alexandra.fresco.framework.network.Network;
-import dk.alexandra.fresco.framework.util.Drbg;
-
 /**
  * Class representing a hash-based commitment. Secure assuming that SHA-256 is a
  * random oracle. An instantiated object represents a commitment by itself and
  * does *not* contain any secret information. An object gets instantiated by
- * calling the commit command.
- *
+ * calling the commit command. <br/>
  * The scheme itself is based on the ROM folklore scheme where the message to
  * commit to is concatenated with a random string and then hashed. The hash
  * digest serves as the commitment itself and the opening is the randomness and
@@ -31,7 +26,9 @@ public class Commitment {
    * The length of the hash digest along with the randomness used.
    */
   public static final int DIGEST_LENGTH = 32; // 256 / 8 bytes
-  // The actual value representing the commitment
+  /**
+   * The actual value representing the commitment.
+   */
   byte[] commitmentVal = null;
   private final MessageDigest digest;
 
@@ -59,12 +56,12 @@ public class Commitment {
     // Sample a sufficient amount of random bits
     byte[] randomness = new byte[DIGEST_LENGTH];
     rand.nextBytes(randomness);
-    // Construct an array to contain the byte to hash
+    // Construct an array to contain the bytes to hash
     byte[] openingInfo = new byte[value.length + randomness.length];
     System.arraycopy(value, 0, openingInfo, 0, value.length);
     System.arraycopy(randomness, 0, openingInfo, value.length,
         randomness.length);
-    commitmentVal = digest.digest(value);
+    commitmentVal = digest.digest(openingInfo);
     return openingInfo;
   }
 
@@ -82,18 +79,15 @@ public class Commitment {
       throw new MaliciousException(
           "The opening info is too small to be a commitment.");
     }
-    // Extract the randomness and the value committed to from the openingInfo
-    // The value comes first
-    byte[] value = new byte[openingInfo.length - DIGEST_LENGTH];
-    System.arraycopy(openingInfo, 0, value, 0, openingInfo.length - DIGEST_LENGTH);
-    // The randomness comes at the end
-    // TODO[TOR] This should be in use?
-    byte[] randomness = new byte[DIGEST_LENGTH];
-    System.arraycopy(openingInfo, value.length, randomness, 0, DIGEST_LENGTH);
     // Hash the opening info and verify that it matches the value stored in
     // "commitmentValue"
-    byte[] digestValue = digest.digest(value);
+    byte[] digestValue = digest.digest(openingInfo);
     if (Arrays.equals(digestValue, commitmentVal)) {
+      // Extract the randomness and the value committed to from the openingInfo
+      // The value comes first
+      byte[] value = new byte[openingInfo.length - DIGEST_LENGTH];
+      System.arraycopy(openingInfo, 0, value, 0,
+          openingInfo.length - DIGEST_LENGTH);
       return value;
     } else {
       throw new MaliciousException("The opening info does not match the commitment.");
