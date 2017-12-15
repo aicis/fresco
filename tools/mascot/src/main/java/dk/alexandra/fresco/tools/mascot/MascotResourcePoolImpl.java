8a62aa74582050fa2f1b8e006d1e86ddd6b5043e
Peter F. Frandsen <pff@partisia.com>
2017-12-15 13:49:42 +0100

    Fixed compile cross test

--- tools/mascot/src/main/java/dk/alexandra/fresco/tools/mascot/MascotResourcePoolImpl.java
+++ tools/mascot/src/main/java/dk/alexandra/fresco/tools/mascot/MascotResourcePoolImpl.java
@@ -14,12 +14,10 @@ import dk.alexandra.fresco.tools.mascot.utils.FieldElementPrgImpl;
 import dk.alexandra.fresco.tools.ot.base.NaorPinkasOt;
 import dk.alexandra.fresco.tools.ot.base.Ot;
 import dk.alexandra.fresco.tools.ot.base.RotBatch;
-import dk.alexandra.fresco.tools.ot.base.TestNaorPinkasOt;
 import dk.alexandra.fresco.tools.ot.otextension.BristolRotBatch;
 import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.util.List;
-
 import javax.crypto.spec.DHParameterSpec;
 
 public class MascotResourcePoolImpl extends ResourcePoolImpl implements MascotResourcePool {
@@ -101,12 +99,32 @@ public class MascotResourcePoolImpl extends ResourcePoolImpl implements MascotRe
     return commitmentSerializer;
   }
 
+  public static final BigInteger DhGvalue = new BigInteger(
+      "1817929693051677794042418360119535939035448877384059423016092223723589389"
+          + "89386921540078076694389023214591116103022506752626702949377742490622411"
+          + "36154252930934999558878557838951366230121689192613836661801579283976804"
+          + "90566221950235571908449465416597162122008963523511429191971262704962062"
+          + "23722995544735685829105160578247097947199471860741139749699562917671426"
+          + "82888600060270321923905677901250333513320663621356005726499527794262632"
+          + "80575136645831734174762968521856711608877942562412558950963899754610266"
+          + "97615963606394464455636761856586890950014177457842992286652934126338664"
+          + "99748366638338849983708609236396436614761807745");
+  public static final BigInteger DhPvalue = new BigInteger(
+      "2080109726332741595567900301553712643291061397185326442939225885200811703"
+          + "46221477943683854922915625754365585955880683687623164529077074421717622"
+          + "55815247681135202838112705300460371527291002353818384380395178484616163"
+          + "81789931732016235932408088148285827220196826505807878031275264842308641"
+          + "84386700540754381703938109115634660390655677474772619937553430208773150"
+          + "06567328507885962926589890627547794887973720401310026543273364787901564"
+          + "85827844212318499978829377355564689095172787513731965744913645190518423"
+          + "06594567246898679968677700656495114013774368779648395287433119164167454"
+          + "67731166272088057888135437754886129005590419051");
+
   @Override
   public RotBatch<StrictBitVector> createRot(int otherId, Network network) {
-    DHParameterSpec params = new DHParameterSpec(TestNaorPinkasOt.DhPvalue,
-        TestNaorPinkasOt.DhGvalue);
+    DHParameterSpec params = new DHParameterSpec(DhPvalue, DhGvalue);
     Ot ot = ExceptionConverter.safe(() -> new NaorPinkasOt(getMyId(), otherId,
-        getRandomGenerator(), network, params),
+            getRandomGenerator(), network, params),
         "Missing security hash function or PRG, which is dependent in this library");
     return new BristolRotBatch(getMyId(), otherId, getModBitLength(), getLambdaSecurityParam(),
         getRandomGenerator(), network, ot);
