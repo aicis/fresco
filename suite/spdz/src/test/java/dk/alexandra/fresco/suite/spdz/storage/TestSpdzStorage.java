f5f32309933b0b602a29a395aaba763881c0e706
damgaard <kasper.damgaard@alexandra.dk>
2017-12-19 16:33:29 +0100

    Moved test class to test files and tested a bit more within DataSupplierImpl

---
+++ suite/spdz/src/test/java/dk/alexandra/fresco/suite/spdz/storage/TestSpdzStorage.java
@@ -0,0 +1,23 @@
+package dk.alexandra.fresco.suite.spdz.storage;
+
+import dk.alexandra.fresco.framework.MPCException;
+import dk.alexandra.fresco.framework.sce.resources.storage.FilebasedStreamedStorageImpl;
+import dk.alexandra.fresco.framework.sce.resources.storage.InMemoryStorage;
+import org.junit.Test;
+
+public class TestSpdzStorage {
+
+  @Test(expected = MPCException.class)
+  public void testDataSupplierModNotFound() {
+    DataSupplierImpl supplier =
+        new DataSupplierImpl(new FilebasedStreamedStorageImpl(new InMemoryStorage()), "invalid", 2);
+    supplier.getModulus();
+  }
+
+  @Test(expected = MPCException.class)
+  public void testDataSupplierSskNotFound() {
+    DataSupplierImpl supplier =
+        new DataSupplierImpl(new FilebasedStreamedStorageImpl(new InMemoryStorage()), "invalid", 2);
+    supplier.getSSK();
+  }
+}
