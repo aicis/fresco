7d2730f8cd76b00c7ae91f2a0d09a03aa9379908
jot2re <33345641+jot2re@users.noreply.github.com>
2018-01-15 16:51:21 +0100

    Added description to the MaliciousException class

--- core/src/main/java/dk/alexandra/fresco/framework/MaliciousException.java
+++ core/src/main/java/dk/alexandra/fresco/framework/MaliciousException.java
@@ -1,6 +1,14 @@
+
 package dk.alexandra.fresco.framework;
 
+/**
+ * Runtime exception to be thrown if and only if the code verifies that another party has acted
+ * maliciously. Thus this should only be thrown when it is indisputable that a party has acted
+ * maliciously.
+ */
 public class MaliciousException extends MPCException {
+
+  private static final long serialVersionUID = -3829588807931116397L;
 
   public MaliciousException(String message) {
     super(message);
