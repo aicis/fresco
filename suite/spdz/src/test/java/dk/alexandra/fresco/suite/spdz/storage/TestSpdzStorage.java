36a12df67bb54ffce3003a52d1331a086275df04
Peter F. Frandsen <pff@partisia.com>
2018-01-16 11:31:16 +0100

    Added tests of error conditions

--- suite/spdz/src/test/java/dk/alexandra/fresco/suite/spdz/storage/TestSpdzStorage.java
+++ suite/spdz/src/test/java/dk/alexandra/fresco/suite/spdz/storage/TestSpdzStorage.java
@@ -9,15 +9,57 @@ public class TestSpdzStorage {
   @Test(expected = IllegalArgumentException.class)
   public void testDataSupplierModNotFound() {
     SpdzStorageDataSupplier supplier =
-        new SpdzStorageDataSupplier(new FilebasedStreamedStorageImpl(new InMemoryStorage()), "invalid", 2);
+        new SpdzStorageDataSupplier(new FilebasedStreamedStorageImpl(new InMemoryStorage()),
+            "invalid", 2);
     supplier.getModulus();
   }
 
   @Test(expected = IllegalArgumentException.class)
   public void testDataSupplierSskNotFound() {
     SpdzStorageDataSupplier supplier =
-        new SpdzStorageDataSupplier(new FilebasedStreamedStorageImpl(new InMemoryStorage()), "invalid", 2);
+        new SpdzStorageDataSupplier(new FilebasedStreamedStorageImpl(new InMemoryStorage()),
+            "invalid", 2);
     supplier.getSecretSharedKey();
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testDataSupplierTripleNotFound() {
+    SpdzStorageDataSupplier supplier =
+        new SpdzStorageDataSupplier(new FilebasedStreamedStorageImpl(new InMemoryStorage()),
+            "invalid", 2);
+    supplier.getNextTriple();
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testDataSupplierExpPipeNotFound() {
+    SpdzStorageDataSupplier supplier =
+        new SpdzStorageDataSupplier(new FilebasedStreamedStorageImpl(new InMemoryStorage()),
+            "invalid", 2);
+    supplier.getNextExpPipe();
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testDataSupplierInputMaskNotFound() {
+    SpdzStorageDataSupplier supplier =
+        new SpdzStorageDataSupplier(new FilebasedStreamedStorageImpl(new InMemoryStorage()),
+            "invalid", 2);
+    supplier.getNextInputMask(1);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testDataSupplierNextBitNotFound() {
+    SpdzStorageDataSupplier supplier =
+        new SpdzStorageDataSupplier(new FilebasedStreamedStorageImpl(new InMemoryStorage()),
+            "invalid", 2);
+    supplier.getNextBit();
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testDataSupplierRandomElmNotFound() {
+    SpdzStorageDataSupplier supplier =
+        new SpdzStorageDataSupplier(new FilebasedStreamedStorageImpl(new InMemoryStorage()),
+            "invalid", 2);
+    supplier.getNextRandomFieldElement();
   }
 
   @Test(expected = IllegalArgumentException.class)
