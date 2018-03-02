7e570dd46a76af95220b87a44d225d364c1eba25
Jonas Lindstrøm <jonas.l.jensen@alexandra.dk>
2018-03-02 11:37:39 +0100

    Remove author

--- tools/fixedpointarithmetic/src/main/java/dk/alexandra/fresco/decimal/fixed/SFixed.java
+++ tools/fixedpointarithmetic/src/main/java/dk/alexandra/fresco/decimal/fixed/SFixed.java
@@ -8,9 +8,6 @@ import dk.alexandra.fresco.framework.value.SInt;
  * Closed datatype for representing binary fixed point numbers, e.g. represent a fraction <i>x</i>
  * as <i>n 2<sup>e</sup></i> where <i>n</i> is an {@link SInt}, <i>e &ge; 0</i> is a precision
  * (avaialble via {@link #getScale()}) that may vary from value to value.
- * 
- * @author Jonas Lindstrøm (jonas.lindstrom@alexandra.dk)
- *
  */
 public class SFixed implements SReal, DRes<SReal> {
   private final DRes<SInt> value;
