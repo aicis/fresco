1364058b7829d672e5ac8243eead889831662719
Nikolaj Volgushev <n1v0lg@users.noreply.github.com>
2018-04-11 11:39:07 +0200

    Add separate native batched tests

--- suite/spdz/src/test/java/dk/alexandra/fresco/suite/spdz/basic/TestSpdzBasicArithmetic.java
+++ suite/spdz/src/test/java/dk/alexandra/fresco/suite/spdz/basic/TestSpdzBasicArithmetic.java
@@ -10,130 +10,130 @@ public class TestSpdzBasicArithmetic extends AbstractSpdzTest {
 
   @Test
   public void testInput() {
-    runTest(new BasicArithmeticTests.TestInput<>(), EvaluationStrategy.SEQUENTIAL_BATCHED);
+    runTest(new BasicArithmeticTests.TestInput<>(), EvaluationStrategy.NATIVE_BATCHED);
   }
 
   @Test
   public void testAddInSequence() {
-    runTest(new BasicArithmeticTests.TestAddInSequence<>(), EvaluationStrategy.SEQUENTIAL_BATCHED);
+    runTest(new BasicArithmeticTests.TestAddInSequence<>(), EvaluationStrategy.NATIVE_BATCHED);
   }
 
   @Test
   public void testAdd() {
-    runTest(new BasicArithmeticTests.TestAdd<>(), EvaluationStrategy.SEQUENTIAL_BATCHED);
+    runTest(new BasicArithmeticTests.TestAdd<>(), EvaluationStrategy.NATIVE_BATCHED);
   }
 
   @Test
   public void testAddWithOverflow() {
     runTest(new BasicArithmeticTests.TestAddWithOverflow<>(),
-        EvaluationStrategy.SEQUENTIAL_BATCHED);
+        EvaluationStrategy.NATIVE_BATCHED);
   }
 
   @Test
   public void testMultiply() {
-    runTest(new BasicArithmeticTests.TestMultiply<>(), EvaluationStrategy.SEQUENTIAL_BATCHED);
+    runTest(new BasicArithmeticTests.TestMultiply<>(), EvaluationStrategy.NATIVE_BATCHED);
   }
 
   @Test
   public void testMultiplyInSequence() {
     runTest(new BasicArithmeticTests.TestMultiplyInSequence<>(),
-        EvaluationStrategy.SEQUENTIAL_BATCHED);
+        EvaluationStrategy.NATIVE_BATCHED);
   }
 
   @Test
   public void testMultiplyByZero() {
-    runTest(new BasicArithmeticTests.TestMultiplyByZero<>(), EvaluationStrategy.SEQUENTIAL_BATCHED);
+    runTest(new BasicArithmeticTests.TestMultiplyByZero<>(), EvaluationStrategy.NATIVE_BATCHED);
   }
 
   @Test
   public void testMultiplyWithOverflow() {
     runTest(new BasicArithmeticTests.TestMultiplyWithOverflow<>(),
-        EvaluationStrategy.SEQUENTIAL_BATCHED);
+        EvaluationStrategy.NATIVE_BATCHED);
   }
 
   @Test
   public void testKnown() {
-    runTest(new BasicArithmeticTests.TestKnownSInt<>(), EvaluationStrategy.SEQUENTIAL_BATCHED);
+    runTest(new BasicArithmeticTests.TestKnownSInt<>(), EvaluationStrategy.NATIVE_BATCHED);
   }
 
   @Test
   public void testAddPublic() {
-    runTest(new BasicArithmeticTests.TestAddPublicValue<>(), EvaluationStrategy.SEQUENTIAL_BATCHED);
+    runTest(new BasicArithmeticTests.TestAddPublicValue<>(), EvaluationStrategy.NATIVE_BATCHED);
   }
 
   @Test
   public void testInputOutputMany() {
-    runTest(new TestCloseAndOpenList<>(), EvaluationStrategy.SEQUENTIAL_BATCHED);
+    runTest(new TestCloseAndOpenList<>(), EvaluationStrategy.NATIVE_BATCHED);
   }
 
   @Test
   public void testMultiplyMany() {
-    runTest(new BasicArithmeticTests.TestLotsMult<>(), EvaluationStrategy.SEQUENTIAL_BATCHED);
+    runTest(new BasicArithmeticTests.TestLotsMult<>(), EvaluationStrategy.NATIVE_BATCHED);
   }
 
   @Test
   public void testSumAndMult() {
-    runTest(new BasicArithmeticTests.TestSumAndMult<>(), EvaluationStrategy.SEQUENTIAL_BATCHED);
+    runTest(new BasicArithmeticTests.TestSumAndMult<>(), EvaluationStrategy.NATIVE_BATCHED);
   }
 
   @Test
   public void testSimpleMultAndAdd() {
     runTest(new BasicArithmeticTests.TestSimpleMultAndAdd<>(),
-        EvaluationStrategy.SEQUENTIAL_BATCHED);
+        EvaluationStrategy.NATIVE_BATCHED);
   }
 
   @Test
   public void testAlternatingMultAdd() {
     runTest(new BasicArithmeticTests.TestAlternatingMultAdd<>(),
-        EvaluationStrategy.SEQUENTIAL_BATCHED);
+        EvaluationStrategy.NATIVE_BATCHED);
   }
 
   @Test
   public void testMultiplyByPublicValue() {
     runTest(new BasicArithmeticTests.TestMultiplyByPublicValue<>(),
-        EvaluationStrategy.SEQUENTIAL_BATCHED);
+        EvaluationStrategy.NATIVE_BATCHED);
   }
 
   @Test
   public void testSubtract() {
     runTest(new BasicArithmeticTests.TestSubtract<>(),
-        EvaluationStrategy.SEQUENTIAL_BATCHED);
+        EvaluationStrategy.NATIVE_BATCHED);
   }
 
   @Test
   public void testSubtractNegative() {
     runTest(new BasicArithmeticTests.TestSubtractNegative<>(),
-        EvaluationStrategy.SEQUENTIAL_BATCHED);
+        EvaluationStrategy.NATIVE_BATCHED);
   }
 
   @Test
   public void testSubtractPublic() {
     runTest(new BasicArithmeticTests.TestSubtractPublic<>(),
-        EvaluationStrategy.SEQUENTIAL_BATCHED);
+        EvaluationStrategy.NATIVE_BATCHED);
   }
 
   @Test
   public void testSubtractFromPublic() {
     runTest(new BasicArithmeticTests.TestSubtractFromPublic<>(),
-        EvaluationStrategy.SEQUENTIAL_BATCHED);
+        EvaluationStrategy.NATIVE_BATCHED);
   }
 
   @Test
   public void testOutputToSingleParty() {
     runTest(new BasicArithmeticTests.TestOutputToSingleParty<>(),
-        EvaluationStrategy.SEQUENTIAL_BATCHED);
+        EvaluationStrategy.NATIVE_BATCHED);
   }
 
   @Test
   public void testRandomBit() {
     runTest(new BasicArithmeticTests.TestRandomBit<>(),
-        EvaluationStrategy.SEQUENTIAL_BATCHED);
+        EvaluationStrategy.NATIVE_BATCHED);
   }
 
   @Test
   public void testRandomElement() {
     runTest(new BasicArithmeticTests.TestRandomElement<>(),
-        EvaluationStrategy.SEQUENTIAL_BATCHED);
+        EvaluationStrategy.NATIVE_BATCHED);
   }
 
 }
