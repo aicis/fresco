d0d32f12d9baf45c73cee1c2dd069962fa5c99f1
Nikolaj Volgushev <n1v0lg@users.noreply.github.com>
2018-04-11 14:58:11 +0200

    Fewer batched native dea solver tests

--- suite/spdz/src/test/java/dk/alexandra/fresco/suite/spdz/batchednative/TestSpdzDEASolver.java
+++ suite/spdz/src/test/java/dk/alexandra/fresco/suite/spdz/batchednative/TestSpdzDEASolver.java
@@ -2,8 +2,6 @@ package dk.alexandra.fresco.suite.spdz.batchednative;
 
 import dk.alexandra.fresco.framework.sce.evaluator.EvaluationStrategy;
 import dk.alexandra.fresco.lib.statistics.DeaSolver.AnalysisType;
-import dk.alexandra.fresco.lib.statistics.DeaSolverTests.RandomDataDeaTest;
-import dk.alexandra.fresco.lib.statistics.DeaSolverTests.TestDeaFixed1;
 import dk.alexandra.fresco.lib.statistics.DeaSolverTests.TestDeaFixed2;
 import dk.alexandra.fresco.suite.spdz.AbstractSpdzTest;
 import dk.alexandra.fresco.suite.spdz.configuration.PreprocessingStrategy;
@@ -14,34 +12,6 @@ public class TestSpdzDEASolver extends AbstractSpdzTest {
   @Test
   public void test_DEASolver_2_Sequential_batched_dummy_minimize_1() {
     runTest(new TestDeaFixed2<>(AnalysisType.INPUT_EFFICIENCY),
-        EvaluationStrategy.NATIVE_BATCHED,
-        PreprocessingStrategy.DUMMY, 2);
-  }
-
-  @Test
-  public void test_DEASolver_2_Sequential_batched_dummy_minimize_2() {
-    runTest(new TestDeaFixed1<>(AnalysisType.INPUT_EFFICIENCY),
-        EvaluationStrategy.NATIVE_BATCHED,
-        PreprocessingStrategy.DUMMY, 2);
-  }
-
-  @Test
-  public void test_DEASolver_2_Sequential_batched_dummy_maximize_fixed_data_1() {
-    runTest(new TestDeaFixed1<>(AnalysisType.OUTPUT_EFFICIENCY),
-        EvaluationStrategy.NATIVE_BATCHED,
-        PreprocessingStrategy.DUMMY, 2);
-  }
-
-  @Test
-  public void test_DEASolver_2_Sequential_batched_dummy_maximize_fixed_data_2() {
-    runTest(new TestDeaFixed2<>(AnalysisType.OUTPUT_EFFICIENCY),
-        EvaluationStrategy.NATIVE_BATCHED,
-        PreprocessingStrategy.DUMMY, 2);
-  }
-
-  @Test
-  public void test_DEASolver_2_Sequential_batched_dummy_maximize() {
-    runTest(new RandomDataDeaTest<>(5, 1, 30, 3, AnalysisType.OUTPUT_EFFICIENCY),
         EvaluationStrategy.NATIVE_BATCHED,
         PreprocessingStrategy.DUMMY, 2);
   }
