d682f3af72f63e9357d4f673b9d085799b604590
Nikolaj Volgushev <n1v0lg@users.noreply.github.com>
2018-04-20 14:21:15 +0200

    Clean up carry out

--- core/src/main/java/dk/alexandra/fresco/lib/compare/lt/CarryOut.java
+++ core/src/main/java/dk/alexandra/fresco/lib/compare/lt/CarryOut.java
@@ -2,11 +2,11 @@ package dk.alexandra.fresco.lib.compare.lt;
 
 import dk.alexandra.fresco.framework.DRes;
 import dk.alexandra.fresco.framework.builder.Computation;
+import dk.alexandra.fresco.framework.builder.numeric.Numeric;
 import dk.alexandra.fresco.framework.builder.numeric.ProtocolBuilderNumeric;
 import dk.alexandra.fresco.framework.util.SIntPair;
 import dk.alexandra.fresco.framework.value.SInt;
 import dk.alexandra.fresco.lib.math.integer.binary.ArithmeticAndKnownRight;
-import dk.alexandra.fresco.lib.math.integer.binary.ArithmeticXorKnownRight;
 import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -18,48 +18,69 @@ import java.util.List;
  */
 public class CarryOut implements Computation<SInt, ProtocolBuilderNumeric> {
 
-  private final DRes<List<DRes<BigInteger>>> clearBits;
-  private final DRes<List<DRes<SInt>>> secretBits;
+  private static final BigInteger TWO = BigInteger.valueOf(2);
+  private final DRes<List<DRes<BigInteger>>> clearBitsDef;
+  private final DRes<List<DRes<SInt>>> secretBitsDef;
   private final BigInteger carryIn;
 
+  /**
+   * Constructs new {@link CarryOut}.
+   *
+   * @param clearBits clear bits
+   * @param secretBits secret bits
+   * @param carryIn an additional carry-in bit which we add to the least-significant bits of the
+   * inputs
+   */
   public CarryOut(DRes<List<DRes<BigInteger>>> clearBits, DRes<List<DRes<SInt>>> secretBits,
       BigInteger carryIn) {
-    this.secretBits = secretBits;
-    this.clearBits = clearBits;
+    this.secretBitsDef = secretBits;
+    this.clearBitsDef = clearBits;
     this.carryIn = carryIn;
   }
 
+  /**
+   * Default call to {@link #CarryOut(DRes, DRes, BigInteger)} without a carry-in.
+   */
   public CarryOut(DRes<List<DRes<BigInteger>>> clearBits, DRes<List<DRes<SInt>>> secretBits) {
     this(clearBits, secretBits, BigInteger.ZERO);
   }
 
   @Override
   public DRes<SInt> buildComputation(ProtocolBuilderNumeric builder) {
-    // TODO both calls should be in parallel
-    // TODO don't re-use generic protocols to cut number of mults in half
-    DRes<List<DRes<SInt>>> xoredDef = builder
-        .par(new ArithmeticXorKnownRight(secretBits, clearBits));
-    DRes<List<DRes<SInt>>> andedDef = builder
-        .par(new ArithmeticAndKnownRight(secretBits, clearBits));
-    DRes<List<DRes<SIntPair>>> pairs = builder.seq(seq -> {
-      List<DRes<SInt>> xored = xoredDef.out();
-      List<DRes<SInt>> anded = andedDef.out();
-      List<DRes<SIntPair>> innerPairs = new ArrayList<>(xored.size());
-      for (int i = 0; i < xored.size() - 1; i++) {
-        SIntPair pair = new SIntPair(xored.get(i), anded.get(i));
-        innerPairs.add(() -> pair);
-      }
-      // need to account for carry-in bit
-      int lastIdx = xored.size() - 1;
-      DRes<SInt> lastCarryPropagator = seq.numeric().add(
-          anded.get(lastIdx),
-          seq.numeric().mult(carryIn, xored.get(lastIdx)));
-      SIntPair pair = new SIntPair(xored.get(lastIdx), lastCarryPropagator);
-      innerPairs.add(() -> pair);
-      Collections.reverse(innerPairs);
-      return () -> innerPairs;
-    });
-    return builder.seq(new PreCarryBits(pairs));
+    List<DRes<SInt>> secretBits = secretBitsDef.out();
+    List<DRes<BigInteger>> clearBits = clearBitsDef.out();
+    if (secretBits.size() != clearBits.size()) {
+      throw new IllegalArgumentException("Number of bits must be the same");
+    }
+    return builder.par(new ArithmeticAndKnownRight(secretBitsDef, clearBitsDef))
+        .par((par, andedBits) -> {
+          List<DRes<SIntPair>> pairs = new ArrayList<>(andedBits.size());
+          for (int i = 0; i < secretBits.size(); i++) {
+            DRes<SInt> leftBit = secretBits.get(i);
+            BigInteger rightBit = clearBits.get(i).out();
+            DRes<SInt> andedBit = andedBits.get(i);
+            // logical xor of two bits can is leftBit + rightBit - 2 * leftBit * rightBit
+            DRes<SInt> xoredBit = par.seq(seq -> {
+              Numeric nb = seq.numeric();
+              return nb.sub(
+                  nb.add(rightBit, leftBit),
+                  nb.mult(TWO, andedBit)
+              );
+            });
+            pairs.add(() -> new SIntPair(xoredBit, andedBit));
+          }
+          return () -> pairs;
+        }).seq((seq, pairs) -> {
+          // need to account for carry-in bit
+          int lastIdx = pairs.size() - 1;
+          SIntPair lastPair = pairs.get(lastIdx).out();
+          DRes<SInt> lastCarryPropagator = seq.numeric().add(
+              lastPair.getSecond(),
+              seq.numeric().mult(carryIn, lastPair.getFirst()));
+          pairs.set(lastIdx, () -> new SIntPair(lastPair.getFirst(), lastCarryPropagator));
+          Collections.reverse(pairs);
+          return seq.seq(new PreCarryBits(() -> pairs));
+        });
   }
 
 }
