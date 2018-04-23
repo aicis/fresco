6a93830c8af24248cd9232fbd31699ddea415daa
Nikolaj Volgushev <n1v0lg@users.noreply.github.com>
2018-04-23 15:11:53 +0200

    Remove generic compuint

--- suite/spdz2k/src/test/java/dk/alexandra/fresco/suite/spdz2k/util/TestUIntSerializer.java
+++ suite/spdz2k/src/test/java/dk/alexandra/fresco/suite/spdz2k/util/TestUIntSerializer.java
@@ -4,9 +4,9 @@ import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 import dk.alexandra.fresco.framework.network.serializers.ByteSerializer;
+import dk.alexandra.fresco.suite.spdz2k.datatypes.CompUInt128;
+import dk.alexandra.fresco.suite.spdz2k.datatypes.CompUInt128Factory;
 import dk.alexandra.fresco.suite.spdz2k.datatypes.CompUIntFactory;
-import dk.alexandra.fresco.suite.spdz2k.datatypes.GenericCompUInt;
-import dk.alexandra.fresco.suite.spdz2k.datatypes.GenericCompUIntFactory;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
@@ -14,8 +14,8 @@ import org.junit.Test;
 
 public class TestUIntSerializer {
 
-  private final CompUIntFactory<GenericCompUInt> factory = new GenericCompUIntFactory(64, 64);
-  private final ByteSerializer<GenericCompUInt> serializer = new UIntSerializer<>(factory);
+  private final CompUIntFactory<CompUInt128> factory = new CompUInt128Factory();
+  private final ByteSerializer<CompUInt128> serializer = new UIntSerializer<>(factory);
 
   @Test
   public void testSerialize() {
@@ -24,7 +24,7 @@ public class TestUIntSerializer {
     rawBytes[1] = 0x02;
     rawBytes[2] = 0x03;
     rawBytes[15] = 0x16;
-    GenericCompUInt element = factory.createFromBytes(rawBytes);
+    CompUInt128 element = factory.createFromBytes(rawBytes);
     assertArrayEquals(rawBytes, serializer.serialize(element));
   }
 
@@ -33,7 +33,7 @@ public class TestUIntSerializer {
     Random random = new Random(42);
     byte[] rawBytes = new byte[32];
     random.nextBytes(rawBytes);
-    List<GenericCompUInt> elements = Arrays.asList(
+    List<CompUInt128> elements = Arrays.asList(
         factory.createFromBytes(Arrays.copyOfRange(rawBytes, 0, 16)),
         factory.createFromBytes(Arrays.copyOfRange(rawBytes, 16, 32))
     );
@@ -46,7 +46,7 @@ public class TestUIntSerializer {
     Random random = new Random(42);
     byte[] bytes = new byte[16];
     random.nextBytes(bytes);
-    GenericCompUInt uint = serializer.deserialize(bytes);
+    CompUInt128 uint = serializer.deserialize(bytes);
     assertArrayEquals(bytes, uint.toByteArray());
   }
 
@@ -55,11 +55,11 @@ public class TestUIntSerializer {
     Random random = new Random(42);
     byte[] rawBytes = new byte[32];
     random.nextBytes(rawBytes);
-    List<GenericCompUInt> expected = Arrays.asList(
+    List<CompUInt128> expected = Arrays.asList(
         factory.createFromBytes(Arrays.copyOfRange(rawBytes, 0, 16)),
         factory.createFromBytes(Arrays.copyOfRange(rawBytes, 16, 32))
     );
-    List<GenericCompUInt> actual = serializer.deserializeList(rawBytes);
+    List<CompUInt128> actual = serializer.deserializeList(rawBytes);
     assertEquals(expected.size(), actual.size());
     for (int i = 0; i < actual.size(); i++) {
       assertArrayEquals(expected.get(i).toByteArray(), actual.get(i).toByteArray());
@@ -68,7 +68,6 @@ public class TestUIntSerializer {
 
   @Test(expected = IllegalArgumentException.class)
   public void testDeserializeListWrongLength() {
-    Random random = new Random(42);
     byte[] rawBytes = new byte[33];
     serializer.deserializeList(rawBytes);
   }
