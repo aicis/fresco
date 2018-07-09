669534b9fefe7ec040e248a762db98a3611c5f1a
Peter Sebastian Nordholt <peter.s.nordholt@alexandra.dk>
2018-07-09 10:46:27 +0200

    Removed redundant code line in AsyncNetwork.java.

--- core/src/main/java/dk/alexandra/fresco/framework/network/AsyncNetwork.java
+++ core/src/main/java/dk/alexandra/fresco/framework/network/AsyncNetwork.java
@@ -145,7 +145,6 @@ public class AsyncNetwork implements CloseableNetwork {
           SocketChannel channel = SocketChannel.open();
           channel.connect(addr);
           channel.configureBlocking(true);
-          channelMap.put(i, channel);
           ByteBuffer b = ByteBuffer.allocate(PARTY_ID_BYTES);
           b.put((byte) conf.getMyId());
           b.position(0);
