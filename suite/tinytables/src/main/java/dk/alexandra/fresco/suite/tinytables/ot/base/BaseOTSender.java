01b92b32f0f4316b98da76109361ebf98aef2867
Peter Sebastian Nordholt <peter.s.nordholt@alexandra.dk>
2018-08-15 00:18:46 +0200

    Removing tabs from BaseOTReceiver and BaseOTSender.

--- suite/tinytables/src/main/java/dk/alexandra/fresco/suite/tinytables/ot/base/BaseOTSender.java
+++ suite/tinytables/src/main/java/dk/alexandra/fresco/suite/tinytables/ot/base/BaseOTSender.java
@@ -15,63 +15,63 @@ import java.util.List;
  * This OTSender is a wrapper around SCAPI's
  * {@link OTSemiHonestDDHBatchOnByteArraySender} based on an elliptic curve over
  * a finite field, namely the K-163 curve.
- * 
+ *
  * @author Jonas Lindstrøm (jonas.lindstrom@alexandra.dk)
  *
  */
 public class BaseOTSender implements dk.alexandra.fresco.suite.tinytables.ot.OTSender {
 
-	private Network network;
-	private int myId;
-	private SecureRandom random;
+  private Network network;
+  private int myId;
+  private SecureRandom random;
 
-	/*
-	 * We keep a singleton of the actual sender.
-	 */
-	private static OTSemiHonestDDHBatchOnByteArraySender sender;
-	private OTSemiHonestDDHBatchOnByteArraySender getInstance(SecureRandom random) {
-		if (sender == null) {
-			try {
-				sender = new OTSemiHonestDDHBatchOnByteArraySender(
-						new edu.biu.scapi.primitives.dlog.bc.BcDlogECF2m(), 
-						//new edu.biu.scapi.primitives.dlog.openSSL.OpenSSLDlogECF2m(), 
-						KdfFactory.getInstance()
-						.getObject("HKDF(HMac(SHA-256))"), random);
-			} catch (SecurityLevelException | FactoriesException | IOException e) {
-				e.printStackTrace();
-			}
-		}
-		
-		return sender;
-	}
-	
-	public BaseOTSender(Network network, int myId, SecureRandom random) {
-		this.network = network;
-		this.myId = myId;
-		this.random = random;
-	}
-	
-	@Override
+  /*
+   * We keep a singleton of the actual sender.
+   */
+  private static OTSemiHonestDDHBatchOnByteArraySender sender;
+  private OTSemiHonestDDHBatchOnByteArraySender getInstance(SecureRandom random) {
+    if (sender == null) {
+      try {
+        sender = new OTSemiHonestDDHBatchOnByteArraySender(
+            new edu.biu.scapi.primitives.dlog.bc.BcDlogECF2m(),
+            //new edu.biu.scapi.primitives.dlog.openSSL.OpenSSLDlogECF2m(),
+            KdfFactory.getInstance()
+            .getObject("HKDF(HMac(SHA-256))"), random);
+      } catch (SecurityLevelException | FactoriesException | IOException e) {
+        e.printStackTrace();
+      }
+    }
+
+    return sender;
+  }
+
+  public BaseOTSender(Network network, int myId, SecureRandom random) {
+    this.network = network;
+    this.myId = myId;
+    this.random = random;
+  }
+
+  @Override
   public void send(List<dk.alexandra.fresco.suite.tinytables.ot.datatypes.OTInput> inputs) {
 
     ArrayList<byte[]> x0 = new ArrayList<byte[]>();
-		ArrayList<byte[]> x1 = new ArrayList<byte[]>();
+    ArrayList<byte[]> x1 = new ArrayList<byte[]>();
     for (dk.alexandra.fresco.suite.tinytables.ot.datatypes.OTInput input : inputs) {
       x0.add(dk.alexandra.fresco.suite.tinytables.ot.Encoding
           .encodeBitSet(input.getX0(), input.getLength()));
       x1.add(dk.alexandra.fresco.suite.tinytables.ot.Encoding
           .encodeBitSet(input.getX1(), input.getLength()));
     }
-		
-		OTBatchOnByteArraySInput otsInputs = new OTBatchOnByteArraySInput(x0, x1);
-		OTSemiHonestDDHBatchOnByteArraySender sender = getInstance(random);
-		
-		try {
-			sender.transfer(new NetworkWrapper(network, myId), otsInputs);
-		} catch (ClassNotFoundException | IOException e) {
-			// Do nothing
-		}
-		
-	}
-	
+
+    OTBatchOnByteArraySInput otsInputs = new OTBatchOnByteArraySInput(x0, x1);
+    OTSemiHonestDDHBatchOnByteArraySender sender = getInstance(random);
+
+    try {
+      sender.transfer(new NetworkWrapper(network, myId), otsInputs);
+    } catch (ClassNotFoundException | IOException e) {
+      // Do nothing
+    }
+
+  }
+
 }
