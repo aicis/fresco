265ad1d11bfbc83be51e8c3b508020097fdd8b31
Nikolaj Volgushev <n1v0lg@users.noreply.github.com>
2018-09-26 11:34:29 +0200

    Re-arrange mod2m protocol

--- core/src/main/java/dk/alexandra/fresco/lib/math/integer/mod/Mod2m.java
+++ core/src/main/java/dk/alexandra/fresco/lib/math/integer/mod/Mod2m.java
@@ -3,9 +3,11 @@ package dk.alexandra.fresco.lib.math.integer.mod;
 import dk.alexandra.fresco.framework.DRes;
 import dk.alexandra.fresco.framework.builder.Computation;
 import dk.alexandra.fresco.framework.builder.numeric.ProtocolBuilderNumeric;
+import dk.alexandra.fresco.framework.util.Pair;
 import dk.alexandra.fresco.framework.value.OInt;
 import dk.alexandra.fresco.framework.value.SInt;
 import dk.alexandra.fresco.lib.math.integer.binary.RandomBitMask;
+import java.util.List;
 
 /**
  * Computes modular reduction of value mod 2^m.
@@ -37,26 +39,25 @@ public class Mod2m implements Computation<SInt, ProtocolBuilderNumeric> {
     if (m >= k) {
       return input;
     }
-    DRes<RandomBitMask> r = builder.advancedNumeric().randomBitMask(k + kappa);
-    // Construct a new RandomBitMask consisting of the first m bits of r
-    DRes<RandomBitMask> rPrime = builder.seq(seq -> seq.advancedNumeric()
-        .randomBitMask(() -> r.out().getBits().out().subList(0, m)));
-
+    final DRes<RandomBitMask> r = builder.advancedNumeric().randomBitMask(k + kappa);
     return builder.seq(seq -> {
+      // Construct a new RandomBitMask consisting of the first m bits of r
+      final List<DRes<SInt>> rBits = r.out().getBits().out().subList(0, m);
+      DRes<RandomBitMask> rPrime = seq.advancedNumeric().randomBitMask(() -> rBits);
       // Use the integer interpretation of r to compute c = 2^{k-1}+(input + r)
       DRes<OInt> c = seq.numeric().openAsOInt(seq.numeric().addOpen(seq
-          .getOIntArithmetic().twoTo(k - 1), seq.numeric().add(input, r.out()
-              .getValue())));
-      return c;
-    }).seq((seq, c) -> {
-      DRes<OInt> cPrime = seq.getOIntArithmetic().modTwoTo(c, m);
+          .getOIntArithmetic().twoTo(k - 1), seq.numeric().add(input, r.out().getValue())));
+      Pair<DRes<RandomBitMask>, DRes<OInt>> pair = new Pair<>(rPrime, c);
+      return () -> pair;
+    }).seq((seq, pair) -> {
+      DRes<RandomBitMask> rPrime = pair.getFirst();
+      DRes<OInt> c = pair.getSecond();
+      DRes<OInt> cPrime = seq.getOIntArithmetic().modTwoTo(c.out(), m);
       DRes<SInt> u = seq.comparison().compareLTBits(cPrime, rPrime.out().getBits());
       // Return cPrime - 2^m * u
       return seq.numeric().add(seq.numeric().multByOpen(seq.getOIntArithmetic()
-          .twoTo(m), u),
+              .twoTo(m), u),
           seq.numeric().subFromOpen(cPrime, rPrime.out().getValue()));
     });
-
   }
-
 }
