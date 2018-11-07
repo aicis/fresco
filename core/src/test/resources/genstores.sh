ddeebad639a42a87e940dbe1d346d6c2a7f8c41c
Peter Sebastian Nordholt <peter.s.nordholt@alexandra.dk>
2018-11-07 14:15:15 +0100

    Updated script to generate key/truststore for TLS tests.

--- core/src/test/resources/genstores.sh
+++ core/src/test/resources/genstores.sh
@@ -6,7 +6,7 @@ for INDEX in `seq $1`
 do 
     ALIAS=P$INDEX
     STORE=keystore$INDEX
-    keytool -genkeypair -alias $ALIAS -keyalg RSA -validity 600 -keystore $STORE -dname "CN=$ALIAS, OU=TLSTest, O=FRESCO, C=DK" -storepass $PASS
+    keytool -genkeypair -alias $ALIAS -keyalg RSA -validity 600 -keystore $STORE -dname "CN=$ALIAS, OU=TLSTest, O=FRESCO, C=DK" -storepass $PASS -ext BC=ca:false
     keytool -export -alias $ALIAS -keystore $STORE -rfc -file $ALIAS.cer -storepass $PASS
     keytool -import -alias $ALIAS-cert -file $ALIAS.cer -keystore truststore -storepass $PASS -noprompt
 done
