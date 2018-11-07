6d1199d2921d7dcbb89d2d47c3c68e48301c811a
Nikolaj Volgushev <n1v0lg@users.noreply.github.com>
2018-11-07 11:07:37 +0100

    Kill generic comp uint

--- suite/spdz2k/src/test/java/dk/alexandra/fresco/suite/spdz2k/resource/storage/TestSpdz2kDummyDataSupplier.java
+++ suite/spdz2k/src/test/java/dk/alexandra/fresco/suite/spdz2k/resource/storage/TestSpdz2kDummyDataSupplier.java
@@ -9,8 +9,8 @@ import dk.alexandra.fresco.suite.spdz2k.datatypes.CompUIntFactory;
 import dk.alexandra.fresco.suite.spdz2k.datatypes.Spdz2kSInt;
 import dk.alexandra.fresco.suite.spdz2k.datatypes.Spdz2kInputMask;
 import dk.alexandra.fresco.suite.spdz2k.datatypes.Spdz2kTriple;
-import dk.alexandra.fresco.suite.spdz2k.datatypes.GenericCompUInt;
-import dk.alexandra.fresco.suite.spdz2k.datatypes.GenericCompUIntFactory;
+import dk.alexandra.fresco.suite.spdz2k.datatypes.CompUInt128;
+import dk.alexandra.fresco.suite.spdz2k.datatypes.CompUInt128Factory;
 import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -20,26 +20,26 @@ import org.junit.Test;
 public class TestSpdz2kDummyDataSupplier {
 
   private void testGetNextRandomElementShare(int noOfParties) {
-    List<Spdz2kDataSupplier<GenericCompUInt>> suppliers = setupSuppliers(noOfParties);
-    GenericCompUInt macKey = getMacKeyFromSuppliers(suppliers);
-    List<Spdz2kSInt<GenericCompUInt>> shares = new ArrayList<>(noOfParties);
-    for (Spdz2kDataSupplier<GenericCompUInt> supplier : suppliers) {
+    List<Spdz2kDataSupplier<CompUInt128>> suppliers = setupSuppliers(noOfParties);
+    CompUInt128 macKey = getMacKeyFromSuppliers(suppliers);
+    List<Spdz2kSInt<CompUInt128>> shares = new ArrayList<>(noOfParties);
+    for (Spdz2kDataSupplier<CompUInt128> supplier : suppliers) {
       shares.add(supplier.getNextRandomElementShare());
     }
-    Spdz2kSInt<GenericCompUInt> recombined = recombine(shares);
+    Spdz2kSInt<CompUInt128> recombined = recombine(shares);
     assertFalse("Random value was 0 ",
         recombined.getShare().toBigInteger().equals(BigInteger.ZERO));
     assertMacCorrect(recombined, macKey);
   }
 
   private void testGetNextBitShare(int noOfParties) {
-    List<Spdz2kDataSupplier<GenericCompUInt>> suppliers = setupSuppliers(noOfParties);
-    GenericCompUInt macKey = getMacKeyFromSuppliers(suppliers);
-    List<Spdz2kSInt<GenericCompUInt>> shares = new ArrayList<>(noOfParties);
-    for (Spdz2kDataSupplier<GenericCompUInt> supplier : suppliers) {
+    List<Spdz2kDataSupplier<CompUInt128>> suppliers = setupSuppliers(noOfParties);
+    CompUInt128 macKey = getMacKeyFromSuppliers(suppliers);
+    List<Spdz2kSInt<CompUInt128>> shares = new ArrayList<>(noOfParties);
+    for (Spdz2kDataSupplier<CompUInt128> supplier : suppliers) {
       shares.add(supplier.getNextBitShare());
     }
-    Spdz2kSInt<GenericCompUInt> recombined = recombine(shares);
+    Spdz2kSInt<CompUInt128> recombined = recombine(shares);
     BigInteger asBitInt = recombined.getShare().toBigInteger();
     assertTrue("Not a bit " + asBitInt,
         asBitInt.equals(BigInteger.ZERO) || asBitInt.equals(BigInteger.ONE));
@@ -47,16 +47,16 @@ public class TestSpdz2kDummyDataSupplier {
   }
 
   private void testGetInputMask(int noOfParties, int towardParty) {
-    List<Spdz2kDataSupplier<GenericCompUInt>> suppliers = setupSuppliers(noOfParties);
-    GenericCompUInt macKey = getMacKeyFromSuppliers(suppliers);
-    List<Spdz2kInputMask<GenericCompUInt>> masks = new ArrayList<>(noOfParties);
-    for (Spdz2kDataSupplier<GenericCompUInt> supplier : suppliers) {
+    List<Spdz2kDataSupplier<CompUInt128>> suppliers = setupSuppliers(noOfParties);
+    CompUInt128 macKey = getMacKeyFromSuppliers(suppliers);
+    List<Spdz2kInputMask<CompUInt128>> masks = new ArrayList<>(noOfParties);
+    for (Spdz2kDataSupplier<CompUInt128> supplier : suppliers) {
       masks.add(supplier.getNextInputMask(towardParty));
     }
-    GenericCompUInt realValue = null;
-    List<Spdz2kSInt<GenericCompUInt>> shares = new ArrayList<>(noOfParties);
+    CompUInt128 realValue = null;
+    List<Spdz2kSInt<CompUInt128>> shares = new ArrayList<>(noOfParties);
     for (int i = 1; i <= noOfParties; i++) {
-      Spdz2kInputMask<GenericCompUInt> inputMask = masks.get(i - 1);
+      Spdz2kInputMask<CompUInt128> inputMask = masks.get(i - 1);
       if (i != towardParty) {
         assertTrue(null == inputMask.getOpenValue());
       } else {
@@ -64,19 +64,19 @@ public class TestSpdz2kDummyDataSupplier {
       }
       shares.add(inputMask.getMaskShare());
     }
-    Spdz2kSInt<GenericCompUInt> recombined = recombine(shares);
+    Spdz2kSInt<CompUInt128> recombined = recombine(shares);
     assertMacCorrect(recombined, macKey);
     assertEquals(realValue.toBigInteger(), recombined.getShare().toBigInteger());
   }
 
   private void testGetNextTripleShares(int noOfParties) {
-    List<Spdz2kDataSupplier<GenericCompUInt>> suppliers = setupSuppliers(noOfParties);
-    GenericCompUInt macKey = getMacKeyFromSuppliers(suppliers);
-    List<Spdz2kTriple<GenericCompUInt>> triples = new ArrayList<>(noOfParties);
-    for (Spdz2kDataSupplier<GenericCompUInt> supplier : suppliers) {
+    List<Spdz2kDataSupplier<CompUInt128>> suppliers = setupSuppliers(noOfParties);
+    CompUInt128 macKey = getMacKeyFromSuppliers(suppliers);
+    List<Spdz2kTriple<CompUInt128>> triples = new ArrayList<>(noOfParties);
+    for (Spdz2kDataSupplier<CompUInt128> supplier : suppliers) {
       triples.add(supplier.getNextTripleShares());
     }
-    Spdz2kTriple<GenericCompUInt> recombined = recombineTriples(triples);
+    Spdz2kTriple<CompUInt128> recombined = recombineTriples(triples);
     assertTripleValid(recombined, macKey);
   }
 
@@ -111,45 +111,45 @@ public class TestSpdz2kDummyDataSupplier {
     testGetNextTripleShares(5);
   }
 
-  private Spdz2kSInt<GenericCompUInt> recombine(
-      List<Spdz2kSInt<GenericCompUInt>> shares) {
+  private Spdz2kSInt<CompUInt128> recombine(
+      List<Spdz2kSInt<CompUInt128>> shares) {
     return shares.stream().reduce(Spdz2kSInt::add).get();
   }
 
-  private void assertMacCorrect(Spdz2kSInt<GenericCompUInt> recombined,
-      GenericCompUInt macKey) {
+  private void assertMacCorrect(Spdz2kSInt<CompUInt128> recombined,
+      CompUInt128 macKey) {
     assertArrayEquals(
         macKey.multiply(recombined.getShare()).toByteArray(),
         recombined.getMacShare().toByteArray()
     );
   }
 
-  private List<Spdz2kDataSupplier<GenericCompUInt>> setupSuppliers(
+  private List<Spdz2kDataSupplier<CompUInt128>> setupSuppliers(
       int noOfParties) {
-    List<Spdz2kDataSupplier<GenericCompUInt>> suppliers = new ArrayList<>(
+    List<Spdz2kDataSupplier<CompUInt128>> suppliers = new ArrayList<>(
         noOfParties);
     for (int i = 0; i < noOfParties; i++) {
-      CompUIntFactory<GenericCompUInt> factory = new GenericCompUIntFactory(64, 64);
-      GenericCompUInt macKeyShare = factory.createRandom();
+      CompUIntFactory<CompUInt128> factory = new CompUInt128Factory();
+      CompUInt128 macKeyShare = factory.createRandom();
       suppliers.add(new Spdz2kDummyDataSupplier<>(i + 1, noOfParties, macKeyShare,
           factory));
     }
     return suppliers;
   }
 
-  private GenericCompUInt getMacKeyFromSuppliers(
-      List<Spdz2kDataSupplier<GenericCompUInt>> suppliers) {
+  private CompUInt128 getMacKeyFromSuppliers(
+      List<Spdz2kDataSupplier<CompUInt128>> suppliers) {
     return suppliers.stream()
         .map(Spdz2kDataSupplier::getSecretSharedKey)
-        .reduce(GenericCompUInt::add).get();
+        .reduce(CompUInt128::add).get();
   }
 
-  private Spdz2kTriple<GenericCompUInt> recombineTriples(
-      List<Spdz2kTriple<GenericCompUInt>> triples) {
-    List<Spdz2kSInt<GenericCompUInt>> left = new ArrayList<>(triples.size());
-    List<Spdz2kSInt<GenericCompUInt>> right = new ArrayList<>(triples.size());
-    List<Spdz2kSInt<GenericCompUInt>> product = new ArrayList<>(triples.size());
-    for (Spdz2kTriple<GenericCompUInt> triple : triples) {
+  private Spdz2kTriple<CompUInt128> recombineTriples(
+      List<Spdz2kTriple<CompUInt128>> triples) {
+    List<Spdz2kSInt<CompUInt128>> left = new ArrayList<>(triples.size());
+    List<Spdz2kSInt<CompUInt128>> right = new ArrayList<>(triples.size());
+    List<Spdz2kSInt<CompUInt128>> product = new ArrayList<>(triples.size());
+    for (Spdz2kTriple<CompUInt128> triple : triples) {
       left.add(triple.getLeft());
       right.add(triple.getRight());
       product.add(triple.getProduct());
@@ -157,7 +157,7 @@ public class TestSpdz2kDummyDataSupplier {
     return new Spdz2kTriple<>(recombine(left), recombine(right), recombine(product));
   }
 
-  private void assertTripleValid(Spdz2kTriple<GenericCompUInt> recombined, GenericCompUInt macKey) {
+  private void assertTripleValid(Spdz2kTriple<CompUInt128> recombined, CompUInt128 macKey) {
     assertMacCorrect(recombined.getLeft(), macKey);
     assertMacCorrect(recombined.getRight(), macKey);
     assertMacCorrect(recombined.getRight(), macKey);
