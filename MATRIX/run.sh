984f576ecb35b1f779831145bb4af7f950102140
Peter Sebastian Nordholt <peter.s.nordholt@alexandra.dk>
2018-11-08 15:30:06 +0100

    Adding MATRIX dir.

---
+++ MATRIX/run.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+JAR=../demos/distance/target/fresco-demo-distance.jar
+
+java -jar $JAR -e SEQUENTIAL_BATCHED -i 1 -p 1:localhost:8081 -p 2:localhost:8082 -s spdz -Dspdz.preprocessingStrategy=DUMMY -x 10 -y 10 > log.txt 2>&1
