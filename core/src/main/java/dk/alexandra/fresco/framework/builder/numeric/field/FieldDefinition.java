2a9f360ece543f2dd8ebcd9bdad8bfa22768c173
Emil Bremer Orloff <ebo@secata.com>
2018-12-14 13:10:06 +0100

    Remove byte serializer methods from FieldDefinition

--- core/src/main/java/dk/alexandra/fresco/framework/builder/numeric/field/FieldDefinition.java
+++ core/src/main/java/dk/alexandra/fresco/framework/builder/numeric/field/FieldDefinition.java
@@ -3,7 +3,6 @@ package dk.alexandra.fresco.framework.builder.numeric.field;
 import dk.alexandra.fresco.framework.network.serializers.ByteSerializer;
 import dk.alexandra.fresco.framework.util.StrictBitVector;
 import java.math.BigInteger;
-import java.util.List;
 
 public interface FieldDefinition extends ByteSerializer<FieldElement> {
 
@@ -20,14 +19,6 @@ public interface FieldDefinition extends ByteSerializer<FieldElement> {
   FieldElement createElement(String value);
 
   FieldElement createElement(BigInteger value);
-
-  FieldElement deserialize(byte[] bytes);
-
-  List<FieldElement> deserializeList(byte[] bytes);
-
-  byte[] serialize(FieldElement fieldElement);
-
-  byte[] serialize(List<FieldElement> fieldElements);
 
   StrictBitVector convertToBitVector(FieldElement fieldElement);
 }
