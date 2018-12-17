971ec8e43830a0b2f68ac7a827ef84b510212431
Emil Bremer Orloff <ebo@secata.com>
2018-12-17 12:37:16 +0100

    Cleanup of TestCmdLineUtil

--- demos/common/src/test/java/dk/alexandra/fresco/demo/TestCmdLineUtil.java
+++ demos/common/src/test/java/dk/alexandra/fresco/demo/TestCmdLineUtil.java
@@ -31,12 +31,17 @@ import java.util.List;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.Option;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class TestCmdLineUtil {
 
+  private static final Logger logger = LoggerFactory.getLogger(TestCmdLineUtil.class);
+
   @Test
   public void testDummyBoolFromCmdLine() {
-    CmdLineUtil<ResourcePoolImpl, ProtocolBuilderBinary> cmd = parseAndCloseNetwork(getArgs(1, "dummybool", "-b", "4048"));
+    CmdLineUtil<ResourcePoolImpl, ProtocolBuilderBinary> cmd = parseAndCloseNetwork("dummybool",
+        "-b", "4048");
     assertTrue(cmd.getEvaluator() instanceof BatchedProtocolEvaluator);
     assertEquals(1, cmd.getNetworkConfiguration().getMyId());
     assertEquals(2, cmd.getNetworkConfiguration().noOfParties());
@@ -47,7 +52,8 @@ public class TestCmdLineUtil {
 
   @Test
   public void testDummyBoolFromCmdLineWithLogging() {
-    CmdLineUtil<ResourcePoolImpl, ProtocolBuilderBinary> cmd = parseAndCloseNetwork(getArgs(1, "dummybool", "-l"));
+    CmdLineUtil<ResourcePoolImpl, ProtocolBuilderBinary> cmd = parseAndCloseNetwork("dummybool",
+        "-l");
     assertTrue(cmd.getNetwork() instanceof NetworkLoggingDecorator);
     assertTrue(cmd.getEvaluator() instanceof EvaluatorLoggingDecorator);
     assertEquals(1, cmd.getNetworkConfiguration().getMyId());
@@ -59,7 +65,8 @@ public class TestCmdLineUtil {
 
   @Test
   public void testDummyAritmeticFromCmdLine() {
-    CmdLineUtil<DummyArithmeticResourcePool, ProtocolBuilderNumeric> cmd = parseAndCloseNetwork(getArgs(1, "dummyarithmetic", "-b", "4048"));
+    CmdLineUtil<DummyArithmeticResourcePool, ProtocolBuilderNumeric> cmd = parseAndCloseNetwork(
+        "dummyarithmetic", "-b", "4048");
     assertTrue(cmd.getEvaluator() instanceof BatchedProtocolEvaluator);
     assertEquals(1, cmd.getNetworkConfiguration().getMyId());
     assertEquals(2, cmd.getNetworkConfiguration().noOfParties());
@@ -70,7 +77,8 @@ public class TestCmdLineUtil {
 
   @Test
   public void testSpdzAritmeticDummyFromCmdLine() {
-    CmdLineUtil<SpdzResourcePool, ProtocolBuilderNumeric> cmd = parseAndCloseNetwork(getArgs(1, "spdz", "-b", "4048", "-D", "spdz.preprocessingStrategy=DUMMY"));
+    CmdLineUtil<SpdzResourcePool, ProtocolBuilderNumeric> cmd = parseAndCloseNetwork("spdz", "-b",
+        "4048", "-D", "spdz.preprocessingStrategy=DUMMY");
     assertTrue(cmd.getEvaluator() instanceof BatchedProtocolEvaluator);
     assertEquals(1, cmd.getNetworkConfiguration().getMyId());
     assertEquals(2, cmd.getNetworkConfiguration().noOfParties());
@@ -79,16 +87,19 @@ public class TestCmdLineUtil {
     assertTrue(cmd.getSce() instanceof SecureComputationEngineImpl);
   }
 
-  @Test(expected=IllegalArgumentException.class)
+  @Test(expected = IllegalArgumentException.class)
   public void testSpdzBadBitLength() {
-    parseAndCloseNetwork(getArgs(1, "spdz", "-b", "4048", "-D", "spdz.preprocessingStrategy=DUMMY", "-D", "spdz.maxBitLength=1"));
+    parseAndCloseNetwork("spdz", "-b", "4048", "-D", "spdz.preprocessingStrategy=DUMMY", "-D",
+        "spdz.maxBitLength=1");
   }
 
   @Test
   public void testSpdzAritmeticStaticFromCmdLine() throws IOException {
-    InitializeStorage.initStreamedStorage(new FilebasedStreamedStorageImpl(new InMemoryStorage()), 2, 1, 1, 1, 1, 1);
+    InitializeStorage
+        .initStreamedStorage(new FilebasedStreamedStorageImpl(new InMemoryStorage()), 2, 1, 1, 1, 1,
+            1);
     CmdLineUtil<SpdzResourcePool, ProtocolBuilderNumeric> cmd =
-        parseAndCloseNetwork(getArgs(1, "spdz", "-b", "4048", "-D", "spdz.preprocessingStrategy=STATIC"));
+        parseAndCloseNetwork("spdz", "-b", "4048", "-D", "spdz.preprocessingStrategy=STATIC");
     assertTrue(cmd.getEvaluator() instanceof BatchedProtocolEvaluator);
     assertEquals(1, cmd.getNetworkConfiguration().getMyId());
     assertEquals(2, cmd.getNetworkConfiguration().noOfParties());
@@ -98,14 +109,14 @@ public class TestCmdLineUtil {
     InitializeStorage.cleanup();
   }
 
-  @Test(expected=IllegalArgumentException.class)
+  @Test(expected = IllegalArgumentException.class)
   public void testSpdzAritmeticBadStrategyFromCmdLine() {
-    parseAndCloseNetwork(getArgs(1, "spdz", "-b", "4048", "-D", "spdz.preprocessingStrategy=NO_STRATEGY"));
+    parseAndCloseNetwork("spdz", "-b", "4048", "-D", "spdz.preprocessingStrategy=NO_STRATEGY");
   }
 
   @Test
   public void testTinyTablesPreproFromCmdLine() {
-    CmdLineUtil<?, ?> cmd = parseAndCloseNetwork(getArgs(1, "tinytablesprepro", "-b", "4048"));
+    CmdLineUtil<?, ?> cmd = parseAndCloseNetwork("tinytablesprepro", "-b", "4048");
     assertTrue(cmd.getEvaluator() instanceof BatchedProtocolEvaluator);
     assertEquals(1, cmd.getNetworkConfiguration().getMyId());
     assertEquals(2, cmd.getNetworkConfiguration().noOfParties());
@@ -116,7 +127,7 @@ public class TestCmdLineUtil {
 
   @Test
   public void testTinyTablesFromCmdLine() {
-    CmdLineUtil<?, ?> cmd = parseAndCloseNetwork(getArgs(1, "tinytables", "-b", "4048"));
+    CmdLineUtil<?, ?> cmd = parseAndCloseNetwork("tinytables", "-b", "4048");
     assertTrue(cmd.getEvaluator() instanceof BatchedProtocolEvaluator);
     assertEquals(1, cmd.getNetworkConfiguration().getMyId());
     assertEquals(2, cmd.getNetworkConfiguration().noOfParties());
@@ -125,62 +136,63 @@ public class TestCmdLineUtil {
     assertTrue(cmd.getSce() instanceof SecureComputationEngineImpl);
   }
 
-  @Test(expected=IllegalArgumentException.class)
+  @Test(expected = IllegalArgumentException.class)
   public void testBadProtocolSuiteFromCmdLine() {
     parseIncorrectArgs(getArgs(1, "not-a-protocolsuite", "-b", "4048"));
   }
 
-  @Test(expected=IllegalArgumentException.class)
+  @Test(expected = IllegalArgumentException.class)
   public void testIncorrectArgs() {
     parseIncorrectArgs("-i", "hej");
   }
 
-  @Test(expected=IllegalArgumentException.class)
+  @Test(expected = IllegalArgumentException.class)
   public void testIncorrectPartyId() {
     parseIncorrectArgs("-s", "dummybool", "-p", "1:localhost:8080", "-i", "hej");
   }
 
-  @Test(expected=IllegalArgumentException.class)
+  @Test(expected = IllegalArgumentException.class)
   public void testPartyIdNonPositive() {
     parseIncorrectArgs("-s", "dummybool", "-p", "1:localhost:8080", "-i", "-1");
   }
 
-  @Test(expected=IllegalArgumentException.class)
+  @Test(expected = IllegalArgumentException.class)
   public void testIncorrectSuite() {
     parseIncorrectArgs("-s", "fail", "-p", "1:localhost:8080", "-i", "1");
   }
 
-  @Test(expected=IllegalArgumentException.class)
+  @Test(expected = IllegalArgumentException.class)
   public void testIncorrectPartyFormatTooShort() {
     parseIncorrectArgs("-s", "dummybool", "-p", "1:localhost", "-i", "1");
   }
 
-  @Test(expected=IllegalArgumentException.class)
+  @Test(expected = IllegalArgumentException.class)
   public void testIncorrectPartyFormatTooLong() {
     parseIncorrectArgs("-s", "dummybool", "-p", "1:localhost:8080:bla:toomuch", "-i", "1");
   }
 
-  @Test(expected=IllegalArgumentException.class)
+  @Test(expected = IllegalArgumentException.class)
   public void testportNumberFormat() {
     parseIncorrectArgs("-s", "dummybool", "-p", "1:localhost:fail", "-i", "1");
   }
 
-  @Test(expected=IllegalArgumentException.class)
+  @Test(expected = IllegalArgumentException.class)
   public void testPartyIdNotInList() {
     parseIncorrectArgs("-s", "dummybool", "-p", "1:localhost:8080", "-i", "3");
   }
 
-  @Test(expected=IllegalArgumentException.class)
+  @Test(expected = IllegalArgumentException.class)
   public void testPartyIdIdentical() {
-    parseIncorrectArgs("-s", "dummybool", "-p", "1:localhost:8080", "-p", "1:localhost:8080", "-i", "3");
+    parseIncorrectArgs("-s", "dummybool", "-p", "1:localhost:8080", "-p", "1:localhost:8080", "-i",
+        "3");
   }
 
-  @Test(expected=IllegalArgumentException.class)
+  @Test(expected = IllegalArgumentException.class)
   public void testIllegalBatchSize() {
     parseIncorrectArgs("-s", "dummybool", "-p", "1:localhost:8080", "-i", "1", "-b", "fail");
   }
 
-  @Test(expected=IllegalArgumentException.class)
+  @Test(expected = IllegalArgumentException.class)
   public void testInvalidEvaluationStrat() {
     parseIncorrectArgs("-s", "dummybool", "-p", "1:localhost:8080", "-i", "1", "-e", "fail");
   }
@@ -197,55 +209,54 @@ public class TestCmdLineUtil {
 
   //Since network connects immediately, we need it to actually connect to avoid waiting for timeout
   private <ResourcePoolT extends ResourcePool,
-  Builder extends ProtocolBuilder> CmdLineUtil<ResourcePoolT, Builder>
-  parseAndCloseNetwork(String[] mainArgs) {
+      Builder extends ProtocolBuilder> CmdLineUtil<ResourcePoolT, Builder>
+  parseAndCloseNetwork(String protocolSuite, String... addedOptions) {
 
-    Thread t1 = new Thread(new Runnable() {
-
-      @Override
-      public void run() {
-        CmdLineUtil<ResourcePoolT, Builder> cmd = new CmdLineUtil<>();
-        cmd.parse(getArgs(2, "dummybool"));
-        try {
-          Thread.sleep(50);
-          cmd.closeNetwork();
-        } catch (IOException e) {
-        } catch (InterruptedException e) {
-        }
-      }
-    });
+    Thread t1 = new Thread(
+        () -> {
+          CmdLineUtil<ResourcePoolT, Builder> cmd = new CmdLineUtil<>();
+          cmd.parse(getArgs(2, protocolSuite, addedOptions));
+          try {
+            Thread.sleep(50);
+            cmd.closeNetwork();
+          } catch (InterruptedException | IOException e) {
+            logger.warn("Exception in thread", e);
+          }
+        });
 
     t1.start();
     CmdLineUtil<ResourcePoolT, Builder> cmd = new CmdLineUtil<>();
-    cmd.parse(mainArgs);
+    cmd.parse(getArgs(1, protocolSuite, addedOptions));
     try {
       cmd.closeNetwork();
     } catch (IOException e) {
+      logger.warn("Exception in network close", e);
     }
     try {
       t1.join(1000);
     } catch (InterruptedException e) {
+      logger.warn("Interrupted while waiting for thread", e);
     }
     return cmd;
   }
 
-  private String[] getArgs(int partyId, String protocolSuite, String...addedOptions) {
-    List<String> defaultArgs = new ArrayList<>();
-    defaultArgs.addAll(Arrays.asList(new String[] { "-e", "SEQUENTIAL_BATCHED", "-i", "" + partyId, "-p", "1:localhost:8081",
-        "-p", "2:localhost:8082", "-s", protocolSuite }));
-    if(addedOptions.length > 0) {
-      for(String opt : addedOptions) {
-        defaultArgs.add(opt);
-      }
-    }
-    return defaultArgs.toArray(new String[] {});
+  private String[] getArgs(int partyId, String protocolSuite, String... addedOptions) {
+    List<String> defaultArgs = new ArrayList<>(
+        Arrays.asList(
+            "-e", "SEQUENTIAL_BATCHED",
+            "-i", "" + partyId,
+            "-p", "1:localhost:8081",
+            "-p", "2:localhost:8082",
+            "-s", protocolSuite)
+    );
+    defaultArgs.addAll(Arrays.asList(addedOptions));
+    return defaultArgs.toArray(new String[]{});
   }
 
-  private void parseIncorrectArgs(String...args) {
+  private void parseIncorrectArgs(String... args) {
 
     @SuppressWarnings("rawtypes")
     CmdLineUtil cmd = new CmdLineUtil<>();
     cmd.parse(args);
   }
-
 }
