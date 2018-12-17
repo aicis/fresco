cd2c90670ddec4f6da5913ed6515f5fd5aa8bb6b
Esben Overlund Andersen <eoa@secata.com>
2018-12-17 11:22:22 +0100

    FieldDefinition coverage partly

--- core/src/test/java/dk/alexandra/fresco/framework/builder/numeric/field/FieldUtilsTest.java
+++ core/src/test/java/dk/alexandra/fresco/framework/builder/numeric/field/FieldUtilsTest.java
@@ -2,7 +2,6 @@ package dk.alexandra.fresco.framework.builder.numeric.field;
 
 import static org.junit.Assert.assertThat;
 
-import dk.alexandra.fresco.framework.util.ModulusFinder;
 import dk.alexandra.fresco.framework.util.StrictBitVector;
 import java.math.BigInteger;
 import java.util.Arrays;
@@ -12,33 +11,51 @@ import org.junit.Test;
 
 public final class FieldUtilsTest {
 
-  private BigIntegerFieldDefinition definition = new BigIntegerFieldDefinition(
-      ModulusFinder.findSuitableModulus(8));
-  private List<FieldElement> elements = Arrays.asList(
-      definition.createElement(0),
-      definition.createElement(definition.getModulus().shiftRight(1)),
-      definition.createElement(definition.getModulus().subtract(BigInteger.ONE))
-  );
+  private BigIntegerFieldDefinition bigIntegerFieldDefinition =
+      new BigIntegerFieldDefinition("340282366920938463463374607431768211283");
+  private MersennePrimeFieldDefinition mersennePrimeFieldDefinition =
+      new MersennePrimeFieldDefinition(128, 173);
   private byte[] bytes = new byte[]{0, 125, -6};
+
+  private List<FieldElement> getElements(FieldDefinition definition) {
+    return Arrays.asList(
+        definition.createElement(0),
+        definition.createElement(definition.getModulus().shiftRight(1)),
+        definition.createElement(definition.getModulus().subtract(BigInteger.ONE))
+    );
+  }
 
   @Test
   public void convertToBitVector() {
-    BigInteger value = definition.getModulus().shiftRight(2);
-    FieldElement fieldElement = definition.createElement(value);
-    StrictBitVector vector = definition.convertToBitVector(fieldElement);
+    BigInteger value = mersennePrimeFieldDefinition.getModulus().shiftRight(2);
+    FieldElement fieldElement = mersennePrimeFieldDefinition.createElement(value);
+    StrictBitVector vector = mersennePrimeFieldDefinition.convertToBitVector(fieldElement);
     StrictBitVector expected = new StrictBitVector(new byte[]{0, 0, 0, 0, 0, 0, 0, 62});
+    assertThat(vector, Is.is(expected));
+
+    value = bigIntegerFieldDefinition.getModulus().shiftRight(2);
+    fieldElement = bigIntegerFieldDefinition.createElement(value);
+    vector = bigIntegerFieldDefinition.convertToBitVector(fieldElement);
+    expected = new StrictBitVector(new byte[]{0, 0, 0, 0, 0, 0, 0, 62});
     assertThat(vector, Is.is(expected));
   }
 
   @Test
   public void serializeList() {
-    byte[] result = definition.serialize(elements);
+    byte[] result = mersennePrimeFieldDefinition
+        .serialize(getElements(mersennePrimeFieldDefinition));
+    assertThat(result, Is.is(bytes));
+
+    result = bigIntegerFieldDefinition.serialize(getElements(bigIntegerFieldDefinition));
     assertThat(result, Is.is(bytes));
   }
 
   @Test
   public void deserializeList() {
-    List<FieldElement> result = definition.deserializeList(bytes);
-    assertThat(result, Is.is(elements));
+    List<FieldElement> result = mersennePrimeFieldDefinition.deserializeList(bytes);
+    assertThat(result, Is.is(getElements(mersennePrimeFieldDefinition)));
+
+    result = bigIntegerFieldDefinition.deserializeList(bytes);
+    assertThat(result, Is.is(getElements(bigIntegerFieldDefinition)));
   }
 }
