edb200e952c8487fcf42cf5d9834270e4f1023ec
Peter F. Frandsen <pff@secata.com>
2019-01-16 13:24:39 +0100

    Fixed test

--- core/src/test/java/dk/alexandra/fresco/framework/builder/numeric/field/BigIntegerFieldElementTest.java
+++ core/src/test/java/dk/alexandra/fresco/framework/builder/numeric/field/BigIntegerFieldElementTest.java
@@ -34,7 +34,7 @@ public class BigIntegerFieldElementTest {
 
   @Test(expected = NullPointerException.class)
   public void nullValue() {
-    BigIntegerFieldElement.create(null, BigInteger.ZERO);
+    BigIntegerFieldElement.create((BigInteger) null, modulus);
   }
 
   @Test
