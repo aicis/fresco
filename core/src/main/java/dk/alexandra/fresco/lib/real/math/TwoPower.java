2b0246557fb9bd077b4ca2b61ff6a1d0b711cfb0
Jonas Lindstrøm <jonas.l.jensen@alexandra.dk>
2019-07-10 11:24:22 +0200

    Fix power of two with negative exponent

--- core/src/main/java/dk/alexandra/fresco/lib/real/math/TwoPower.java
+++ core/src/main/java/dk/alexandra/fresco/lib/real/math/TwoPower.java
@@ -44,18 +44,18 @@ public class TwoPower implements Computation<SReal, ProtocolBuilderNumeric> {
       RealNumeric rn = seq.realNumeric();
       
       // Result if exponent is positive
-      DRes<SReal> bp = rn.add(1, rn.fromSInt(bits.get(0)));
-      
-      // Result if exponent is negative
-      DRes<SReal> bn = bp;
+      DRes<SReal> bp = seq.realNumeric().known(1.0);
 
-      // 0'th iteration is the initialization of bp and bn
-      for (int i = 1; i < l; i++) {
+      // Result if exponent is negative
+      DRes<SReal> bn = seq.realNumeric().known(1.0);
+      
+      // TODO: 0'th iteration can be done in init to avoid shifts
+      for (int i = 0; i < l; i++) {
         double twoI = Math.pow(2, i);
         
         DRes<SInt> t = bits.get(i);
-        bp = rn.add(bp, new Scaling(rn.sub(rn.mult(Math.pow(2, twoI), bp), bp), t).buildComputation(seq));
-        bn = rn.add(bn, new Scaling(rn.sub(rn.mult(Math.pow(2, -twoI), bn), bn), t).buildComputation(seq));
+        bp = rn.add(bp, new Scaling(rn.sub(rn.mult(Math.pow(2.0, twoI), bp), bp), t).buildComputation(seq));
+        bn = rn.add(bn, new Scaling(rn.sub(rn.mult(Math.pow(2.0, -twoI), bn), bn), t).buildComputation(seq));
       }
       
       return seq.realAdvanced().condSelect(b, bn, bp);      
