88629446f52e648f1a6e87f0febd8368571e8000
n1v0lg <n1v0lg@users.noreply.github.com>
2019-09-20 15:37:08 +0200

    Sneaky code cov fix

--- core/src/test/java/dk/alexandra/fresco/lib/generic/BroadcastValidationProtocolTest.java
+++ core/src/test/java/dk/alexandra/fresco/lib/generic/BroadcastValidationProtocolTest.java
@@ -1,5 +1,8 @@
 package dk.alexandra.fresco.lib.generic;
 
+import dk.alexandra.fresco.framework.NativeProtocol.EvaluationStatus;
+import dk.alexandra.fresco.framework.sce.resources.ResourcePool;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class BroadcastValidationProtocolTest {
@@ -7,5 +10,22 @@ public class BroadcastValidationProtocolTest {
   @Test(expected = IllegalStateException.class)
   public void testOutThrows() {
     new BroadcastValidationProtocol(new byte[1]).out();
+  }
+
+  @Test
+  public void testEvaluateWithTwoPartiesReturnsDone() {
+    EvaluationStatus status = new BroadcastValidationProtocol<>(new byte[1])
+        .evaluate(0, new ResourcePool() {
+          @Override
+          public int getMyId() {
+            return 0;
+          }
+
+          @Override
+          public int getNoOfParties() {
+            return 2;
+          }
+        }, null);
+    Assert.assertEquals(EvaluationStatus.IS_DONE, status);
   }
 }
