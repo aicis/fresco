cf3030690dabd26462e043b155a7a6ae0099b886
Jonas Lindstrøm <jonas.lindstrom@alexandra.dk>
2021-11-01 21:30:07 +0100

    Use norm to compute bit length

--- lib/common/src/main/java/dk/alexandra/fresco/lib/common/math/integer/binary/BitLength.java
+++ lib/common/src/main/java/dk/alexandra/fresco/lib/common/math/integer/binary/BitLength.java
@@ -32,29 +32,10 @@ public class BitLength implements Computation<SInt, ProtocolBuilderNumeric> {
 
   @Override
   public DRes<SInt> buildComputation(ProtocolBuilderNumeric builder) {
-    return builder.seq((seq) -> {
-      AdvancedNumeric advancedNumeric = AdvancedNumeric.using(seq);
-      //Find the bit representation of the input
-      return advancedNumeric.toBits(input, maxBitLength);
-    }).seq((seq, bits) -> {
-      DRes<SInt> mostSignificantBitIndex = null;
-      Numeric numeric = seq.numeric();
-      for (int n = 0; n < maxBitLength; n++) {
-        // If bits[n] == 1 we let mostSignificantIndex be current index.
-        // Otherwise we leave it be.
-        SInt remainderResult = bits.get(n);
-        if (mostSignificantBitIndex == null) {
-          mostSignificantBitIndex = numeric.mult(BigInteger.valueOf(n), () -> remainderResult);
-        } else {
-          DRes<SInt> sub = numeric.sub(BigInteger.valueOf(n), mostSignificantBitIndex);
-          DRes<SInt> mult = numeric.mult(() -> remainderResult, sub);
-          mostSignificantBitIndex = numeric.add(mult, mostSignificantBitIndex);
-        }
-      }
-      // We are interested in the bit length of the input, so we add one to
-      // the index of the most significant bit since the indices are counte from 0
-      return numeric.add(BigInteger.ONE, mostSignificantBitIndex);
-    });
+    return builder.seq(
+      new NormalizeSInt(input, maxBitLength)).seq((seq, result) ->
+        seq.numeric().sub(maxBitLength, result.getSecond())
+    );
   }
 
 }
