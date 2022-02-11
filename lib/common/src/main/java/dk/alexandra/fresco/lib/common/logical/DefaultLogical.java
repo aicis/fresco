6a77c73a3bd1ac720a997037151136689a7bfb23
Jonas Lindstrøm <jonas.lindstrom@alexandra.dk>
2022-02-11 11:47:36 +0100

    Merge logical computation lib

---
+++ lib/common/src/main/java/dk/alexandra/fresco/lib/common/logical/DefaultLogical.java
@@ -0,0 +1,214 @@
+package dk.alexandra.fresco.lib.common.logical;
+
+import dk.alexandra.fresco.framework.DRes;
+import dk.alexandra.fresco.framework.builder.numeric.ProtocolBuilderNumeric;
+import dk.alexandra.fresco.framework.value.SInt;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.BiFunction;
+import java.util.stream.Collectors;
+
+/**
+ * Default implementation of {@link Logical}, expressing logical operations via arithmetic.
+ */
+public class DefaultLogical implements Logical {
+
+  protected final ProtocolBuilderNumeric builder;
+
+  DefaultLogical(ProtocolBuilderNumeric builder) {
+    this.builder = builder;
+  }
+
+  @Override
+  public DRes<SInt> and(DRes<SInt> bitA, DRes<SInt> bitB) {
+    return builder.seq(seq -> seq.numeric().mult(bitA, bitB));
+  }
+
+  @Override
+  public DRes<SInt> or(DRes<SInt> bitA, DRes<SInt> bitB) {
+    // bitA + bitB - bitA * bitB
+    return builder.seq(seq -> {
+      // mult and add could be in parallel
+      DRes<SInt> sum = seq.numeric().add(bitA, bitB);
+      DRes<SInt> prod = seq.numeric().mult(bitA, bitB);
+      return seq.numeric().sub(sum, prod);
+    });
+  }
+
+  @Override
+  public DRes<SInt> halfOr(DRes<SInt> bitA, DRes<SInt> bitB) {
+    return builder.numeric().add(bitA, bitB);
+  }
+
+  @Override
+  public DRes<SInt> xor(DRes<SInt> bitA, DRes<SInt> bitB) {
+    // knownBit + secretBit - 2 * knownBit * secretBit
+    return builder.seq(seq -> {
+      // mult and add could be in parallel
+      DRes<SInt> sum = seq.numeric().add(bitA, bitB);
+      DRes<SInt> prod = seq.numeric()
+          .mult(BigInteger.valueOf(2), seq.numeric().mult(bitA, bitB));
+      return seq.numeric().sub(sum, prod);
+    });
+  }
+
+  @Override
+  public DRes<SInt> andKnown(BigInteger knownBit, DRes<SInt> secretBit) {
+    return builder.seq(seq -> seq.numeric().mult(knownBit, secretBit));
+  }
+
+  @Override
+  public DRes<SInt> xorKnown(BigInteger knownBit, DRes<SInt> secretBit) {
+    // knownBit + secretBit - 2 * knownBit * secretBit
+    return builder.seq(seq -> {
+      // mult and add could be in parallel
+      DRes<SInt> sum = seq.numeric().add(knownBit, secretBit);
+      DRes<SInt> prod = seq.numeric()
+          .mult(BigInteger.valueOf(2), seq.numeric().mult(knownBit, secretBit));
+      return seq.numeric().sub(sum, prod);
+    });
+  }
+
+  @Override
+  public DRes<SInt> not(DRes<SInt> secretBit) {
+    // 1 - secretBit
+    return builder.seq(seq -> seq.numeric().sub(BigInteger.ONE, secretBit));
+  }
+
+  @Override
+  public DRes<BigInteger> openAsBit(DRes<SInt> secretBit) {
+    return builder.numeric().open(secretBit);
+  }
+
+  @Override
+  public DRes<List<DRes<BigInteger>>> openAsBits(DRes<List<DRes<SInt>>> secretBits) {
+    return builder.par(par -> {
+      List<DRes<BigInteger>> openList =
+          secretBits.out().stream().map(closed -> Logical.using(par).openAsBit(closed))
+              .collect(Collectors.toList());
+      return DRes.of(openList);
+    });
+  }
+
+  @Override
+  public DRes<List<DRes<SInt>>> batchedNot(DRes<List<DRes<SInt>>> bits) {
+    return builder.par(par -> {
+      List<DRes<SInt>> negated =
+          bits.out().stream().map(closed -> Logical.using(par).not(closed))
+              .collect(Collectors.toList());
+      return DRes.of(negated);
+    });
+  }
+
+  private DRes<List<DRes<SInt>>> pairWise(
+      DRes<List<DRes<SInt>>> bitsA,
+      DRes<List<DRes<SInt>>> bitsB,
+      BiFunction<DRes<SInt>, DRes<SInt>, DRes<SInt>> op) {
+    List<DRes<SInt>> leftOut = bitsB.out();
+    List<DRes<SInt>> rightOut = bitsA.out();
+    List<DRes<SInt>> resultBits = new ArrayList<>(leftOut.size());
+    for (int i = 0; i < leftOut.size(); i++) {
+      DRes<SInt> leftBit = leftOut.get(i);
+      DRes<SInt> rightBit = rightOut.get(i);
+      DRes<SInt> resultBit = op.apply(leftBit, rightBit);
+      resultBits.add(resultBit);
+    }
+    return DRes.of(resultBits);
+  }
+
+  private DRes<List<DRes<SInt>>> pairWiseKnown(
+      DRes<List<BigInteger>> knownBits,
+      DRes<List<DRes<SInt>>> secretBits,
+      BiFunction<BigInteger, DRes<SInt>, DRes<SInt>> op) {
+    List<BigInteger> knownOut = knownBits.out();
+    List<DRes<SInt>> secretOut = secretBits.out();
+    List<DRes<SInt>> resultBits = new ArrayList<>(secretOut.size());
+    for (int i = 0; i < secretOut.size(); i++) {
+      DRes<SInt> secretBit = secretOut.get(i);
+      BigInteger knownBit = knownOut.get(i);
+      DRes<SInt> resultBit = op.apply(knownBit, secretBit);
+      resultBits.add(resultBit);
+    }
+    return DRes.of(resultBits);
+  }
+
+  @Override
+  public DRes<List<DRes<SInt>>> pairWiseXorKnown(DRes<List<BigInteger>> knownBits,
+      DRes<List<DRes<SInt>>> secretBits) {
+    return builder.par(par -> {
+      BiFunction<BigInteger, DRes<SInt>, DRes<SInt>> f = (left, right) -> Logical.using(par)
+          .xorKnown(left, right);
+      return pairWiseKnown(knownBits, secretBits, f);
+    });
+  }
+
+  @Override
+  public DRes<List<DRes<SInt>>> pairWiseAndKnown(DRes<List<BigInteger>> knownBits,
+      DRes<List<DRes<SInt>>> secretBits) {
+    return builder.par(par -> {
+      BiFunction<BigInteger, DRes<SInt>, DRes<SInt>> f = (left, right) -> Logical.using(par)
+          .andKnown(left, right);
+      return pairWiseKnown(knownBits, secretBits, f);
+    });
+  }
+
+  @Override
+  public DRes<List<DRes<SInt>>> pairWiseAnd(DRes<List<DRes<SInt>>> bitsA,
+      DRes<List<DRes<SInt>>> bitsB) {
+    return builder.par(par -> {
+      BiFunction<DRes<SInt>, DRes<SInt>, DRes<SInt>> f = (left, right) -> Logical.using(par)
+          .and(left, right);
+      return pairWise(bitsA, bitsB, f);
+    });
+  }
+
+  @Override
+  public DRes<List<DRes<SInt>>> pairWiseOr(DRes<List<DRes<SInt>>> bitsA,
+      DRes<List<DRes<SInt>>> bitsB) {
+    return builder.par(par -> {
+      BiFunction<DRes<SInt>, DRes<SInt>, DRes<SInt>> f = (left, right) -> Logical.using(par)
+          .or(left, right);
+      return pairWise(bitsA, bitsB, f);
+    });
+  }
+
+  @Override
+  public DRes<List<DRes<SInt>>> pairWiseXor(DRes<List<DRes<SInt>>> bitsA,
+      DRes<List<DRes<SInt>>> bitsB) {
+    return builder.par(par -> {
+      BiFunction<DRes<SInt>, DRes<SInt>, DRes<SInt>> f = (left, right) -> Logical.using(par)
+          .xor(left, right);
+      return pairWise(bitsA, bitsB, f);
+    });
+  }
+
+  @Override
+  public DRes<SInt> orOfList(DRes<List<DRes<SInt>>> bits) {
+    return builder.seq(seq -> bits)
+        .whileLoop((inputs) -> inputs.size() > 1,
+            (prevSeq, inputs) -> Logical.using(prevSeq).orNeighbors(inputs))
+        // end while
+        .seq((builder, out) -> out.get(0));
+  }
+
+  @Override
+  public DRes<List<DRes<SInt>>> orNeighbors(List<DRes<SInt>> bits) {
+    return builder.par(par -> {
+      List<DRes<SInt>> out = new ArrayList<>();
+      DRes<SInt> left = null;
+      for (DRes<SInt> currentInput : bits) {
+        if (left == null) {
+          left = currentInput;
+        } else {
+          out.add(Logical.using(par).or(left, currentInput));
+          left = null;
+        }
+      }
+      if (left != null) {
+        out.add(left);
+      }
+      return DRes.of(out);
+    });
+  }
+}
---
+++ lib/common/src/main/java/dk/alexandra/fresco/lib/common/logical/Logical.java
@@ -0,0 +1,121 @@
+package dk.alexandra.fresco.lib.common.logical;
+
+import dk.alexandra.fresco.framework.DRes;
+import dk.alexandra.fresco.framework.builder.ComputationDirectory;
+import dk.alexandra.fresco.framework.builder.numeric.ProtocolBuilderNumeric;
+import dk.alexandra.fresco.framework.value.SInt;
+import java.math.BigInteger;
+import java.util.List;
+
+/**
+ * Logical operators on secret arithmetic representations of boolean values. <p>NOTE: all inputs are
+ * assumed to represent 0 or 1 values only. The result is undefined if other values are passed
+ * in.</p>
+ */
+public interface Logical extends ComputationDirectory {
+  // TODO: this is starting to look a lot like the Binary computation directory...
+
+  public static Logical using(ProtocolBuilderNumeric builder) {
+    return new DefaultLogical(builder);
+  }
+
+  /**
+   * Computes logical AND of inputs. <p>NOTE: Inputs must represent 0 or 1 values only.</p>
+   */
+  DRes<SInt> and(DRes<SInt> bitA, DRes<SInt> bitB);
+
+  /**
+   * Computes logical OR of inputs. <p>NOTE: Inputs must represent 0 or 1 values only.</p>
+   */
+  DRes<SInt> or(DRes<SInt> bitA, DRes<SInt> bitB);
+
+  /**
+   * Computes logical OR of inputs but is only safe to use when at least one of the bits is 0.
+   * <p>This allows to express and or as a linear operation and therefore far more efficient.</p>
+   */
+  DRes<SInt> halfOr(DRes<SInt> bitA, DRes<SInt> bitB);
+
+  /**
+   * Computes logical XOR of inputs. <p>NOTE: Inputs must represent 0 or 1 values only.</p>
+   */
+  DRes<SInt> xor(DRes<SInt> bitA, DRes<SInt> bitB);
+
+  /**
+   * Computes logical AND of inputs. <p>NOTE: Inputs must represent 0 or 1 values only.</p>
+   */
+  DRes<SInt> andKnown(BigInteger knownBit, DRes<SInt> secretBit);
+
+  /**
+   * Computes logical XOR of inputs. <p>NOTE: Inputs must represent 0 or 1 values only.</p>
+   */
+  DRes<SInt> xorKnown(BigInteger knownBit, DRes<SInt> secretBit);
+
+  /**
+   * Computes logical NOT of input. <p>NOTE: Input must represent 0 or 1 values only.</p>
+   */
+  DRes<SInt> not(DRes<SInt> secretBit);
+
+  /**
+   * Opens secret bits, possibly performing conversion before producing final open value. <p>NOTE:
+   * Input must represent 0 or 1 values only.</p>
+   */
+  DRes<BigInteger> openAsBit(DRes<SInt> secretBit);
+
+  /**
+   * Batch opening of bits.
+   */
+  DRes<List<DRes<BigInteger>>> openAsBits(DRes<List<DRes<SInt>>> secretBits);
+
+  /**
+   * Negates all given bits.
+   */
+  DRes<List<DRes<SInt>>> batchedNot(DRes<List<DRes<SInt>>> bits);
+
+  /**
+   * Computes pairwise logical AND of input bits. <p>NOTE: Inputs must represent 0 or 1 values
+   * only.</p>
+   */
+  DRes<List<DRes<SInt>>> pairWiseAndKnown(DRes<List<BigInteger>> knownBits,
+      DRes<List<DRes<SInt>>> secretBits);
+
+  /**
+   * Computes pairwise logical AND of input bits. <p>NOTE: Inputs must represent 0 or 1 values
+   * only.</p>
+   */
+  DRes<List<DRes<SInt>>> pairWiseAnd(DRes<List<DRes<SInt>>> bitsA,
+      DRes<List<DRes<SInt>>> bitsB);
+
+  /**
+   * Computes pairwise logical OR of input bits. <p>NOTE: Inputs must represent 0 or 1 values
+   * only.</p>
+   */
+  DRes<List<DRes<SInt>>> pairWiseOr(DRes<List<DRes<SInt>>> bitsA,
+      DRes<List<DRes<SInt>>> bitsB);
+
+  /**
+   * Computes pairwise logical XOR of input bits. <p>NOTE: Inputs must represent 0 or 1 values
+   * only.</p>
+   */
+  DRes<List<DRes<SInt>>> pairWiseXor(DRes<List<DRes<SInt>>> bitsA,
+      DRes<List<DRes<SInt>>> bitsB);
+
+  /**
+   * Computes pairwise logical XOR of input bits. <p>NOTE: Inputs must represent 0 or 1 values
+   * only.</p>
+   */
+  DRes<List<DRes<SInt>>> pairWiseXorKnown(DRes<List<BigInteger>> knownBits,
+      DRes<List<DRes<SInt>>> secretBits);
+
+  /**
+   * Computes logical OR of all input bits. <p> NOTE: Inputs must represent 0 or 1 values only.
+   * </p>
+   */
+  DRes<SInt> orOfList(DRes<List<DRes<SInt>>> bits);
+
+  /**
+   * Given a list of bits, computes or of each neighbor pair of bits, i.e., given b1, b2, b3, b4,
+   * will output b1 OR b2, b3 OR b4. <p>Also handles uneven number of elements.</p>
+   */
+  DRes<List<DRes<SInt>>> orNeighbors(List<DRes<SInt>> bits);
+
+}
