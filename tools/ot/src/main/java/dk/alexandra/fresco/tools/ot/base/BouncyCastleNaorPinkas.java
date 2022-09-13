38eec185f221650aad442ad284f99e55e35cd735
Mikkel Wienberg Madsen <mikkel.wienberg@alexandra.dk>
2022-09-13 14:06:18 +0200

    WIP computePoint for curve25519

--- tools/ot/src/main/java/dk/alexandra/fresco/tools/ot/base/BouncyCastleNaorPinkas.java
+++ tools/ot/src/main/java/dk/alexandra/fresco/tools/ot/base/BouncyCastleNaorPinkas.java
@@ -2,13 +2,18 @@ package dk.alexandra.fresco.tools.ot.base;
 
 import dk.alexandra.fresco.framework.network.Network;
 import dk.alexandra.fresco.framework.util.Drbg;
+
+import java.security.SecureRandom;
 import java.security.Security;
 import org.bouncycastle.asn1.x9.X9ECParameters;
 import org.bouncycastle.crypto.ec.CustomNamedCurves;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.bouncycastle.math.ec.ECCurve;
+import org.bouncycastle.math.ec.ECFieldElement;
+import org.bouncycastle.math.ec.ECPoint;
 
 import java.math.BigInteger;
+import java.security.spec.ECField;
 
 /**
  * Needs the Bouncy Castle dependency from the OT pom.xml
@@ -26,6 +31,7 @@ public class BouncyCastleNaorPinkas extends AbstractNaorPinkasOT<BouncyCastleECC
 
   private final ECCurve curve;
 
+
   public BouncyCastleNaorPinkas(int otherId, Drbg randBit, Network network) {
     super(otherId, randBit, network);
     Security.addProvider(new BouncyCastleProvider());
@@ -37,8 +43,36 @@ public class BouncyCastleNaorPinkas extends AbstractNaorPinkasOT<BouncyCastleECC
 
   @Override
   BouncyCastleECCElement generateRandomNaorPinkasElement() {
-    return new BouncyCastleECCElement(
-        this.dhGenerator.multiply(this.randNum.nextBigInteger(this.dhModulus)));
+    return new BouncyCastleECCElement(computePoint(this.randNum.nextBigInteger(this.dhModulus)));
+  }
+
+  // Specific for curve25519 in Montgomery form
+  private ECPoint computePoint(BigInteger integer) {
+    ECFieldElement x = this.curve.fromBigInteger(integer);
+    ECFieldElement a = this.curve.getA();
+    ECFieldElement ySquare, y;
+    ECPoint resPoint;
+    do {
+      x = x.addOne();
+      //      = x^3 + ax^2 + x
+      ySquare = x.multiply(x.square()).add(a.multiply(x.square())).add(x);
+
+//      BigInteger x1 = x.toBigInteger();
+//      ySquare = this.curve.fromBigInteger(
+//              x1.modPow(BigInteger.valueOf(3), this.dhModulus)
+//                      .add(a.toBigInteger().multiply(x1.modPow(BigInteger.valueOf(2), this.dhModulus)))
+//                      .add(x1).mod(this.dhModulus)
+//      );
+
+      y = ySquare.sqrt();
+    } while (y == null);
+    resPoint = this.curve.createPoint(x.toBigInteger(), y.toBigInteger()).normalize();
+
+    if (!resPoint.isValid()) {
+      throw new IllegalStateException("EC Point is not a valid curve25519 point");
+    }
+
+    return resPoint;
   }
 
   @Override
@@ -54,5 +88,16 @@ public class BouncyCastleNaorPinkas extends AbstractNaorPinkasOT<BouncyCastleECC
   @Override
   BouncyCastleECCElement getDhGenerator() {
     return new BouncyCastleECCElement(this.dhGenerator);
+  }
+
+  private static BigInteger calculateResidue(BigInteger p) {
+    int bitLength = p.bitLength();
+    if (bitLength >= 96) {
+      BigInteger firstWord = p.shiftRight(bitLength - 64);
+      if (firstWord.longValue() == -1L) {
+        return BigInteger.ONE.shiftLeft(bitLength).subtract(p);
+      }
+    }
+    return BigInteger.ZERO;
   }
 }
