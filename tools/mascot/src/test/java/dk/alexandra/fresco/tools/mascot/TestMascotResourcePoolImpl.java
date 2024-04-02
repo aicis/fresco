e765ffd221a539f9fb6822114227954cf1879884
Mikkel Wienberg Madsen <mikkel.wienberg@alexandra.dk>
2024-04-02 15:41:08 +0200

    Change Map.of to new HashMap

--- tools/mascot/src/test/java/dk/alexandra/fresco/tools/mascot/TestMascotResourcePoolImpl.java
+++ tools/mascot/src/test/java/dk/alexandra/fresco/tools/mascot/TestMascotResourcePoolImpl.java
@@ -3,8 +3,9 @@ package dk.alexandra.fresco.tools.mascot;
 import dk.alexandra.fresco.framework.builder.numeric.field.BigIntegerFieldDefinition;
 import dk.alexandra.fresco.framework.util.AesCtrDrbg;
 import dk.alexandra.fresco.framework.util.ModulusFinder;
-import java.util.Map;
 import org.junit.Test;
+
+import java.util.HashMap;
 
 public class TestMascotResourcePoolImpl {
 
@@ -12,7 +13,7 @@ public class TestMascotResourcePoolImpl {
   @Test(expected = IllegalArgumentException.class)
   public void testCreateRotForSelf() {
     MascotResourcePool resourcePool = new MascotResourcePoolImpl(1, 1, 1,
-        new AesCtrDrbg(new byte[32]), Map.of(), new MascotSecurityParameters(),
+        new AesCtrDrbg(new byte[32]), new HashMap<>(), new MascotSecurityParameters(),
         new BigIntegerFieldDefinition(ModulusFinder.findSuitableModulus(128)));
     resourcePool.createRot(1, null);
   }
