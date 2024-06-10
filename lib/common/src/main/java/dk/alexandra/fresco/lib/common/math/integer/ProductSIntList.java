8be14b33eec5cd8799554b3d171ce685d36b6490
Jon Michael Aanes <jon.michael.aanes@secata.com>
2024-06-10 12:45:05 +0200

    Fallback for zero case

--- lib/common/src/main/java/dk/alexandra/fresco/lib/common/math/integer/ProductSIntList.java
+++ lib/common/src/main/java/dk/alexandra/fresco/lib/common/math/integer/ProductSIntList.java
@@ -7,46 +7,58 @@ import dk.alexandra.fresco.framework.builder.numeric.ProtocolBuilderNumeric;
 import dk.alexandra.fresco.framework.value.SInt;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 /**
- * ComputationBuilder for multiplying a list of SInts.
+ * Builder for a {@link Computation}s that computes the product of a list of {@link SInts}
+ * secret-shared values.
+ *
+ * <p>Empty lists are allowed, and will always produce one.
  */
 public class ProductSIntList implements Computation<SInt, ProtocolBuilderNumeric> {
 
   private final List<DRes<SInt>> input;
 
   /**
-   * Creates a new ProductSIntList.
+   * Creates a new {@link ProductSIntList}.
    *
-   * @param list the list to sum
+   * @param list the list to sum. Not nullable.
    */
   public ProductSIntList(List<DRes<SInt>> list) {
-    input = list;
+    input = Objects.requireNonNull(list);
   }
 
   @Override
   public DRes<SInt> buildComputation(ProtocolBuilderNumeric iterationBuilder) {
-    return iterationBuilder.seq(seq ->
-        () -> input
-    ).whileLoop(
-        (inputs) -> inputs.size() > 1,
-        (seq, inputs) -> seq.par(parallel -> {
-          List<DRes<SInt>> out = new ArrayList<>();
-          Numeric numericBuilder = parallel.numeric();
-          DRes<SInt> left = null;
-          for (DRes<SInt> input1 : inputs) {
-            if (left == null) {
-              left = input1;
-            } else {
-              out.add(numericBuilder.mult(left, input1));
-              left = null;
-            }
-          }
-          if (left != null) {
-            out.add(left);
-          }
-          return () -> out;
-        })
-    ).seq((builder, currentInput) -> currentInput.get(0));
+    // Fast case if there is nothing to compute.
+    if (input.isEmpty()) {
+      return iterationBuilder.seq(seq -> seq.numeric().known(1));
+    }
+
+    // Slow case when there are elements to compute on.
+    return iterationBuilder
+        .seq(seq -> () -> input)
+        .whileLoop(
+            (inputs) -> inputs.size() > 1,
+            (seq, inputs) ->
+                seq.par(
+                    parallel -> {
+                      List<DRes<SInt>> out = new ArrayList<>();
+                      Numeric numericBuilder = parallel.numeric();
+                      DRes<SInt> left = null;
+                      for (DRes<SInt> input1 : inputs) {
+                        if (left == null) {
+                          left = input1;
+                        } else {
+                          out.add(numericBuilder.mult(left, input1));
+                          left = null;
+                        }
+                      }
+                      if (left != null) {
+                        out.add(left);
+                      }
+                      return () -> out;
+                    }))
+        .seq((builder, currentInput) -> currentInput.get(0));
   }
 }
--- lib/common/src/main/java/dk/alexandra/fresco/lib/common/math/integer/SumSIntList.java
+++ lib/common/src/main/java/dk/alexandra/fresco/lib/common/math/integer/SumSIntList.java
@@ -7,46 +7,57 @@ import dk.alexandra.fresco.framework.builder.numeric.ProtocolBuilderNumeric;
 import dk.alexandra.fresco.framework.value.SInt;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 /**
- * ComputationBuilder for summing a list of SInts.
+ * Builder for a {@link Computation}s that sum lists of {@link SInts} secret-shared values.
+ *
+ * <p>Empty lists are allowed, and will always produce zero.
  */
 public class SumSIntList implements Computation<SInt, ProtocolBuilderNumeric> {
 
   private final List<DRes<SInt>> input;
 
   /**
-   * Creates a new SumSIntList.
+   * Creates a new {@link SumSIntList}.
    *
-   * @param list the list to sum
+   * @param list the list to sum. Not nullable.
    */
   public SumSIntList(List<DRes<SInt>> list) {
-    input = list;
+    input = Objects.requireNonNull(list);
   }
 
   @Override
   public DRes<SInt> buildComputation(ProtocolBuilderNumeric iterationBuilder) {
-    return iterationBuilder.seq(seq ->
-        () -> input
-    ).whileLoop(
-        (inputs) -> inputs.size() > 1,
-        (seq, inputs) -> seq.par(parallel -> {
-          List<DRes<SInt>> out = new ArrayList<>();
-          Numeric numericBuilder = parallel.numeric();
-          DRes<SInt> left = null;
-          for (DRes<SInt> input1 : inputs) {
-            if (left == null) {
-              left = input1;
-            } else {
-              out.add(numericBuilder.add(left, input1));
-              left = null;
-            }
-          }
-          if (left != null) {
-            out.add(left);
-          }
-          return () -> out;
-        })
-    ).seq((builder, currentInput) -> currentInput.get(0));
+    // Fast case if there is nothing to sum.
+    if (input.isEmpty()) {
+      return iterationBuilder.seq(seq -> seq.numeric().known(0));
+    }
+
+    // Slow case when there are elements to sum.
+    return iterationBuilder
+        .seq(seq -> () -> input)
+        .whileLoop(
+            (inputs) -> inputs.size() > 1,
+            (seq, inputs) ->
+                seq.par(
+                    parallel -> {
+                      List<DRes<SInt>> out = new ArrayList<>();
+                      Numeric numericBuilder = parallel.numeric();
+                      DRes<SInt> left = null;
+                      for (DRes<SInt> input1 : inputs) {
+                        if (left == null) {
+                          left = input1;
+                        } else {
+                          out.add(numericBuilder.add(left, input1));
+                          left = null;
+                        }
+                      }
+                      if (left != null) {
+                        out.add(left);
+                      }
+                      return () -> out;
+                    }))
+        .seq((builder, currentInput) -> currentInput.get(0));
   }
 }
